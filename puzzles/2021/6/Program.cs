7146aafa54285daaec9ff208d986ba9b6b7b8fb9
Anton Rademaker <anton@freshblue.nl>
2021-12-06 11:54:05 +0100

    Dag 6, optimalisaties

--- puzzles/2021/6/Program.cs
+++ puzzles/2021/6/Program.cs
@@ -1,33 +1,38 @@
 ﻿
-var input = System.IO.File.ReadAllText("input/input.txt");
+var input = System.IO.File.ReadAllText("input/example.txt");
 
 var lanternfish = input.Split(',').Select(x => int.Parse(x)).ToList();
 
-var generations = Enumerable.Range(0,9).Select(_ => 0L).ToList();
+const int maxGeneration = 8;
+const int puzzle1Days = 80;
+const int puzzle2Days = 256;
 
-var maxGeneration = 8;
+var generations = new long[maxGeneration + 1];
 
-for(var generation = 0; generation < maxGeneration; generation++) { 
+for (var generation = 0; generation < maxGeneration; generation++)
+{
     generations[generation] = lanternfish.Count(x => x == generation);
 }
 
 Console.WriteLine(string.Join(',', generations));
 
-Calculate(generations, 80);
+Calculate(generations, puzzle1Days);
 
 Console.WriteLine($"Part 1: Total lanternfishes: {generations.Sum()}");
 
-Calculate(generations, 256-80);
+Calculate(generations, puzzle2Days - puzzle1Days);
 
 Console.WriteLine($"Part 2: Total lanternfishes: {generations.Sum()}");
 
-static void Calculate(List<long> generations, int days)
+static void Calculate(long[] generations, int days)
 {
+    long toSpawn;
     for (int day = 0; day < days; day++)
     {
-        var toSpawn = generations[0];
-        generations.RemoveAt(0);
-        generations.Add(toSpawn);
+        toSpawn = generations[0];
+        System.Buffer.BlockCopy(generations, 1, generations, 0, maxGeneration);
+
+        generations[maxGeneration] = toSpawn;
         generations[6] += toSpawn;
     }
 }
