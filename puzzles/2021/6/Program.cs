f5bd5a9cbc387d73a93251b905153fdfa5d7a6f9
Anton Rademaker <anton@freshblue.nl>
2021-12-06 11:13:29 +0100

    Dag 6

---
+++ puzzles/2021/6/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var input = System.IO.File.ReadAllText("input/input.txt");
+
+var lanternfish = input.Split(',').Select(x => int.Parse(x)).ToList();
+
+var generations = Enumerable.Range(0,9).Select(_ => 0L).ToList();
+
+var maxGeneration = 8;
+
+for(var generation = 0; generation < maxGeneration; generation++) { 
+    generations[generation] = lanternfish.Count(x => x == generation);
+}
+
+Console.WriteLine(string.Join(',', generations));
+
+Calculate(generations, 80);
+
+Console.WriteLine($"Part 1: Total lanternfishes: {generations.Sum()}");
+
+Calculate(generations, 256-80);
+
+Console.WriteLine($"Part 2: Total lanternfishes: {generations.Sum()}");
+
+static void Calculate(List<long> generations, int days)
+{
+    for (int day = 0; day < days; day++)
+    {
+        var toSpawn = generations[0];
+        generations.RemoveAt(0);
+        generations.Add(toSpawn);
+        generations[6] += toSpawn;
+    }
+}
