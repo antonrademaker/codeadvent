d1ae41ab0b466d7c63015f62c2f62ac78f50479d
Anton Rademaker <anton@freshblue.nl>
2021-12-13 10:51:57 +0100

    Day 13

---
+++ puzzles/2021/13/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Immutable;
+using System.Text;
+using System.Text.RegularExpressions;
+
+var inputFiles = new string[] { "input/example.txt", "input/input.txt" };
+
+foreach (var exampleFile in inputFiles/*.Skip(2).Take(1)*/)
+{
+    Console.WriteLine(exampleFile);
+    var file = File.ReadAllLines(exampleFile);
+    CalculatePart1(file);
+    CalculatePart2(file);
+    Console.WriteLine("-- End of file--");
+
+}
+
+void CalculatePart1(string[] lines)
+{
+    var paper = new Paper();
+    paper.Read(lines);
+    paper.Fold();
+    Console.WriteLine($"Part 1: Visible dots: {paper.Positions.Count()}");
+}
+
+void CalculatePart2(string[] lines)
+{
+    var paper = new Paper();
+    paper.Read(lines);
+    while (paper.Fold()) ;
+    paper.Print();
+
+    Console.WriteLine($"Visible dots: {paper.Positions.Count()}");
+}
+
+public class Paper
+{
+    public HashSet<Point> Positions { get; private set; } = new();
+
+    private Queue<string> FoldsToApply = new();
+
+    public void Read(string[] lines)
+    {
+        var readingPositions = true;
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                readingPositions = false;
+                continue;
+            }
+
+            if (readingPositions)
+            {
+                var pos = line.Split(',').Select(int.Parse).ToArray();
+                Positions.Add(new Point(pos[0], pos[1]));
+            }
+            else
+            {
+                FoldsToApply.Enqueue(line);
+            }
+        }
+    }
+
+    public bool Fold()
+    {
+        if (FoldsToApply.TryDequeue(out var fold)) {
+
+            var foldInstruction = fold.Split(' ')[2].Split('=');
+
+            var axis = foldInstruction[0];
+            var value = int.Parse(foldInstruction[1]);
+
+            Console.WriteLine($"Fold along {axis} {value}");
+
+            Func<Point, bool> test = axis == "y" ? (Point p) => p.Y > value : (Point p) => p.X > value;
+            Func<Point, Point> flipper = axis == "y" ? (Point p) => p with { Y = value * 2 - p.Y } : (Point p) => p with { X = value * 2 - p.X };
+
+            var pointsToFlip = Positions.Where(x => test(x)).ToList();
+
+            foreach(var point in pointsToFlip)
+            {
+                Positions.Remove(point);
+                Positions.Add(flipper(point));
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void Print()
+    {
+        var sb = new StringBuilder();
+
+        var columns = Positions.Select(p => p.X).Max();
+        var rows = Positions.Select(p => p.Y).Max();
+
+        for (int y = 0; y <= rows; y++)
+        {
+            for(int x = 0; x <= columns; x++)
+            {
+                if (Positions.Contains(new Point(x,y))) {
+                    sb.Append('#');
+                } else
+                {
+                    sb.Append('.');
+                }
+            }
+            sb.AppendLine();
+        }
+
+        Console.WriteLine(sb.ToString());
+
+    }
+}
+
+public record Point(int X, int Y);
