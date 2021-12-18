a46db867ac1ab092358dd3b66a4891760e2b246d
Anton Rademaker <info@antonrademaker.com>
2021-12-18 20:31:49 +0100

    Dag 18

---
+++ puzzles/2021/18/CalculatorTests.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Day18;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_Two()
+    {
+        var p1 = new Pair(
+            new Pair(1, 1),
+            new Pair(2, 1),
+            0);
+
+        // [[3,4],5]
+        var p2 = new Pair(
+            new Pair(
+                new Pair(3, 2),
+                new Pair(4, 2),
+                1
+            ),
+            new Pair(5, 1),
+            0
+            );
+
+        var result = Calculator.Add(p1, p2);
+
+        var expected = new Pair(
+            new Pair(
+                new Pair(1, 2),
+                new Pair(2, 2),
+                1
+            ),
+            new Pair(
+                new Pair(
+                    new Pair(3, 3),
+                    new Pair(4, 3),
+                    1
+                ),
+                new Pair(5, 2),
+                    1
+                ),
+            0
+            );
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Parse_Single()
+    {
+        var input = "[1,2]";
+
+        var expected = new Pair(
+            new Pair(1, 1),
+            new Pair(2, 1),
+            0);
+
+        var result = Calculator.Parse(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Parse_Complex()
+    {
+        var input = "[[3,4],5]";
+
+        var expected = new Pair(
+             new Pair(
+                 new Pair(3, 2),
+                 new Pair(4, 2),
+                 1
+             ),
+             new Pair(5, 1),
+             0
+             );
+
+        var result = Calculator.Parse(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Parse_Number()
+    {
+        var input = "12";
+
+        var expected = new Pair(12, 0);
+        var result = Calculator.Parse(input);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Print_Number()
+    {
+        var input = new Pair(12, 0);
+        var sb = new StringBuilder();
+        Calculator.Print(input, sb);
+        var result = sb.ToString();
+
+        Assert.Equal("12", result);
+    }
+
+
+    [Fact]
+    public void Print_Single()
+    {
+        var input = new Pair(
+        new Pair(1, 1),
+        new Pair(2, 1),
+        0);
+        var sb = new StringBuilder();
+        Calculator.Print(input, sb);
+        var result = sb.ToString();
+
+        Assert.Equal("[1,2]", result);
+    }
+
+    [Fact]
+    public void Print_Complex()
+    {
+
+        var input = new Pair(
+             new Pair(
+                 new Pair(3, 2),
+                 new Pair(4, 2),
+                 1
+             ),
+             new Pair(5, 1),
+             0
+             );
+
+
+        var sb = new StringBuilder();
+        Calculator.Print(input, sb);
+        var result = sb.ToString();
+
+        Assert.Equal("[[3,4],5]", result);
+    }
+
+    public static TheoryData<string, string, int, int, bool> ExplodeData =>
+    new TheoryData<string, string, int, int, bool>
+        {
+        {"[1,2]","[1,2]", 0,0, false },
+        {"[[[[[9,8],1],2],3],4]","[[[[0,9],2],3],4]", 9,0, true },
+        {"[7,[6,[5,[4,[3,2]]]]]","[7,[6,[5,[7,0]]]]", 0,2, true },
+        {"[[6,[5,[4,[3,2]]]],1]","[[6,[5,[7,0]]],3]", 0,0, true},
+        {"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]","[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", 0,0, true },
+        {"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]","[[3,[2,[8,0]]],[9,[5,[7,0]]]]", 0,2, true },
+        };
+
+    [Theory]
+    [MemberData(nameof(ExplodeData))]
+    public void Explode_Simple(string inp, string expected, int leftExpected, int rightExpected, bool updateExpected)
+    {
+        var input = Calculator.Parse(inp);
+
+        var (result, left, right, updated) = Calculator.Explode(input);
+
+        var output = Calculator.Print(result);
+
+        Assert.Equal(expected, output);
+        Assert.Equal(leftExpected, left);
+        Assert.Equal(rightExpected, right);
+        Assert.Equal(updateExpected, updated);
+    }
+    [Fact]
+    public void Explode_Simple2()
+    {
+        var input =
+             new Pair(
+                 new Pair(9, 5),
+                 new Pair(8, 5),
+                 4
+             );
+
+        var (result, left, right, updated) = Calculator.Explode(input);
+
+        Assert.True(updated);
+        Assert.Equal(9, left);
+        Assert.Equal(8, right);
+
+        var expected = new Pair(0, 4);
+
+        Assert.Equal(expected, result);
+
+    }
+
+    [Fact]
+    public void Explode_Simple3()
+    {
+        var input =
+            new Pair(
+             new Pair(
+                 new Pair(9, 5),
+                 new Pair(8, 5),
+                 4
+             ),
+             new Pair(2, 4),
+             3)
+             ;
+
+        var (result, left, right, updated) = Calculator.Explode(input);
+
+        Assert.True(updated);
+        Assert.Equal(9, left);
+        Assert.Equal(0, right);
+
+        var expected = new Pair(new Pair(0, 4), new Pair(10, 4), 3);
+
+        Assert.Equal(expected, result);
+
+    }
+
+
+    public static TheoryData<string[], string> AddsData =>
+    new TheoryData<string[], string>
+        {
+        { new [] { "[1,1]",
+"[2,2]",
+"[3,3]",
+"[4,4]"
+        }, "[[[[1,1],[2,2]],[3,3]],[4,4]]" },
+        {
+        new []
+        {
+"[1,1]",
+"[2,2]",
+"[3,3]",
+"[4,4]",
+"[5,5]"
+        }, "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+        }, {
+        new []
+        {
+"[1,1]",
+"[2,2]",
+"[3,3]",
+"[4,4]",
+"[5,5]",
+"[6,6]"
+        }, "[[[[5,0],[7,4]],[5,5]],[6,6]]"
+        },
+        { new [] { "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]", "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]" }, "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]" },
+
+{ new [] { "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]" }, "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]" },
+
+{ new [] { "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]", "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]" }, "[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]" },
+
+{ new [] { "[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]", "[7,[5,[[3,8],[1,4]]]]" }, "[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]" },
+
+{ new [] { "[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]", "[[2,[2,2]],[8,[8,1]]]" }, "[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]" },
+
+{ new [] { "[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]", "[2,9]" }, "[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]" },
+
+{ new [] { "[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]", "[1,[[[9,3],9],[[9,0],[0,7]]]]" }, "[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]" },
+
+{ new [] { "[[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]", "[[[5,[7,4]],7],1]" }, "[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]" },
+
+{ new [] { "[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]", "[[[[4,2],2],6],[8,7]]" }, "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]" },
+
+        {
+            new [] {
+"[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+"[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+"[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+"[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+"[7,[5,[[3,8],[1,4]]]]",
+"[[2,[2,2]],[8,[8,1]]]",
+"[2,9]",
+"[1,[[[9,3],9],[[9,0],[0,7]]]]",
+"[[[5,[7,4]],7],1]",
+"[[[[4,2],2],6],[8,7]]"
+        }, "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
+        }
+        };
+
+    [Theory]
+    [MemberData(nameof(AddsData))]
+    public void Adds(string[] pairs, string expected)
+    {
+        var result = Calculator.Parse(pairs[0]);
+        for (var index = 1; index < pairs.Length; index++)
+        {
+            result = Calculator.Add(result, Calculator.Parse(pairs[index]));
+        }
+
+
+        Assert.Equal(expected, Calculator.Print(result));
+    }
+
+
+
+    public static TheoryData<string, long> MagnitudeData =>
+    new TheoryData<string, long>
+    {
+        { "[[1,2],[[3,4],5]]", 143L },
+        { "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384L },
+{ "[[[[1,1],[2,2]],[3,3]],[4,4]]", 445L },
+{ "[[[[3,0],[5,3]],[4,4]],[5,5]]", 791L },
+{ "[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137L },
+{ "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488L }
+    };
+
+
+
+    [Theory]
+    [MemberData(nameof(MagnitudeData))]
+    public void Magnitude(string input, long expected)
+    {
+        var parsed = Calculator.Parse(input);
+        var calculated = Calculator.CalculateMagnitude(parsed);
+        Assert.Equal(expected, calculated);
+    }
+
+}
+
+
+
+
---
+++ puzzles/2021/18/Program.cs
@@ -0,0 +1,345 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+var inputFiles = new string[] {
+    "input/example.txt",
+    "input/input.txt"
+};
+var sw = new Stopwatch();
+
+
+foreach (var exampleFile in inputFiles)
+{
+    Console.WriteLine(exampleFile);
+    var file = File.ReadAllLines(exampleFile);
+    sw.Start();
+
+    CalculatePart1(file);
+
+    sw.Stop();
+    Console.WriteLine($"Part 1 in {sw.ElapsedMilliseconds}ms");
+    sw.Reset();
+    sw.Start();
+
+    CalculatePart2(file);
+
+    sw.Stop();
+    Console.WriteLine($"Part 2 in {sw.ElapsedMilliseconds}ms");
+
+    Console.WriteLine("-- End of file--");
+}
+
+void CalculatePart1(string[] line)
+{
+    var p = Calculator.Parse(line[0]);
+    for (var index = 1; index < line.Length; index++)
+    {
+        p = Calculator.Add(p, Calculator.Parse(line[index]));
+    }
+    Console.WriteLine(Calculator.Print(p));
+    Console.WriteLine($"Magnitude: {Calculator.CalculateMagnitude(p)}");
+}
+
+void CalculatePart2(string[] line)
+{
+    var parsed = line.Select(Calculator.Parse).ToArray();
+
+    var max = 0L;
+
+    for (var i = 0; i < line.Length; i++)
+    {
+        for (var j = i + 1; j < line.Length; j++)
+        {
+            var mag = Calculator.CalculateMagnitude(Calculator.Add(parsed[i], parsed[j]));
+            if (mag > max)
+            {
+                max = mag;
+            }
+        }
+    }
+
+    Console.WriteLine($"Max mag: {max}");
+}
+
+public sealed record Pair
+{
+    public Pair(int number, int depth)
+    {
+        Number = number;
+        Depth = depth;
+    }
+
+    public Pair(Pair left, Pair right, int depth)
+    {
+        ValueLeft = left;
+        ValueRight = right;
+        Depth = depth;
+    }
+
+    public Pair? ValueLeft { get; init; }
+    public Pair? ValueRight { get; init; }
+    public int? Number { get; init; }
+
+    public int Depth { get; init; }
+
+    public bool Equals(Pair? other) => other != null &&
+        ((ValueLeft == null && other.ValueLeft == null) || (ValueLeft != null && other.ValueLeft != null && ValueLeft.Equals(other.ValueLeft))) &&
+        ((ValueRight == null && other.ValueRight == null) || (ValueRight != null && other.ValueRight != null && ValueRight.Equals(other.ValueRight))) &&
+        Number == other.Number;
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
+
+public static class Calculator
+{
+    public static Pair Add(Pair pair1, Pair pair2)
+    {
+        return Reduce(new Pair(IncreaseDepth(pair1), IncreaseDepth(pair2), 0));
+    }
+
+    public static int CalculateMagnitude(Pair pair)
+    {
+        if (pair.Number.HasValue)
+        {
+            return pair.Number.Value;
+        }
+
+        return CalculateMagnitude(pair.ValueLeft!) * 3 + CalculateMagnitude(pair.ValueRight!) * 2;
+    }
+
+    public static Pair Reduce(Pair pair)
+    {
+        var hasChanges = true;
+
+        while (hasChanges)
+        {
+            hasChanges = HasExploded(pair, out pair);
+            if (!hasChanges)
+            {
+                hasChanges = HasSplitted(pair, out pair);
+            }
+        }
+        return pair;
+    }
+
+    public static Pair IncreaseDepth(Pair pair)
+    {
+        if (pair.Number.HasValue)
+        {
+            return pair with
+            {
+                Depth = pair.Depth + 1
+            };
+        }
+        else
+        {
+            return pair with
+            {
+                ValueLeft = IncreaseDepth(pair.ValueLeft!),
+                ValueRight = IncreaseDepth(pair.ValueRight!),
+                Depth = pair.Depth + 1
+            };
+        }
+    }
+
+    public static Pair Parse(string input)
+    {
+        var position = 0;
+        return Parse(input.AsSpan(), ref position, 0);
+    }
+
+    private static Pair Parse(ReadOnlySpan<char> input, ref int currentPosition, int depth)
+    {
+        if (char.IsDigit(input[currentPosition]))
+        {
+            // parse number
+            var start = currentPosition;
+
+            while (currentPosition < input.Length && char.IsDigit(input[currentPosition]))
+            {
+                currentPosition++;
+            }
+            
+            var numberValue = int.Parse(input[start..currentPosition]);
+
+            return new Pair(numberValue, depth);
+        }
+
+        // consume [
+        currentPosition++;
+
+        var left = Parse(input, ref currentPosition, depth + 1);
+        // consume ,
+        currentPosition++;
+
+
+        var right = Parse(input, ref currentPosition, depth + 1);
+        // consume ]
+        currentPosition++;
+
+        return new Pair(left, right, depth);
+    }
+
+    public static string Print(Pair input)
+    {
+        var sb = new StringBuilder();
+        Print(input, sb);
+        return sb.ToString();
+    }
+    public static void Print(Pair input, StringBuilder sb)
+    {
+        if (input.Number.HasValue)
+        {
+            sb.Append(input.Number);
+        }
+        else
+        {
+            sb.Append('[');
+            Print(input.ValueLeft!, sb);
+            sb.Append(',');
+            Print(input.ValueRight!, sb);
+            sb.Append(']');
+        }
+    }
+
+    public static bool HasExploded(Pair input, out Pair output)
+    {
+        var (updated, _, _, hasExploded) = Explode(input);
+        output = updated;
+        return hasExploded;
+    }
+
+    public static (Pair updated, int left, int right, bool hasChanged) Explode(Pair input)
+    {
+        if (input.Number.HasValue)
+        {
+            return (input, 0, 0, false);
+        }
+        else
+        {
+            if (input.Depth == 4)
+            {
+                // explode 
+                return (new Pair(0, 4), input.ValueLeft!.Number!.Value, input.ValueRight!.Number!.Value, true);
+            }
+            else
+            {
+                var (u1, l1, r1, c1) = Explode(input.ValueLeft!);
+
+                if (c1)
+                {
+                    if (r1 > 0)
+                    {
+                        var newRight = AddLeft(input.ValueRight!, r1);
+                        return (input with
+                        {
+                            ValueLeft = u1,
+                            ValueRight = newRight,
+                        }, l1, 0, true);
+                    }
+                    return (input with
+                    {
+                        ValueLeft = u1,
+                    }, l1, r1, true);
+
+                }
+                else
+                {
+                    var (u2, l2, r2, c2) = Explode(input.ValueRight!);
+
+                    if (c2)
+                    {
+                        if (l2 > 0)
+                        {
+                            return (input with
+                            {
+                                ValueLeft = AddRight(input.ValueLeft!, l2),
+                                ValueRight = u2,
+                            }, 0, r2, true);
+                        }
+                        return (input with
+                        {
+                            ValueRight = u2,
+                        }, l2, r2, true);
+                    }
+                    return (input, 0, 0, false);
+                }
+            }
+        }
+    }
+
+    public static Pair AddLeft(Pair pair, int number)
+    {
+        if (pair.ValueLeft != null)
+        {
+            var newLeft = AddLeft(pair.ValueLeft, number);
+            return pair with
+            {
+                ValueLeft = newLeft,
+            };
+        }
+        else
+        {
+            return pair with { Number = pair.Number + number };
+        }
+    }
+
+    public static Pair AddRight(Pair pair, int number)
+    {
+        if (pair.ValueRight != null)
+        {
+            var newRight = AddRight(pair.ValueRight, number);
+            return pair with
+            {
+                ValueRight = newRight
+            };
+        }
+        else
+        {
+            return pair with { Number = pair.Number + number };
+        }
+    }
+
+    public static bool HasSplitted(Pair input, out Pair output)
+    {
+        var (updated, hasSplitted) = Split(input);
+        output = updated;
+        return hasSplitted;
+    }
+
+    public static (Pair updated, bool splitted) Split(Pair input)
+    {
+        if (input.Number.HasValue)
+        {
+            if (input.Number.Value >= 10)
+            {
+                var half = input.Number.Value / 2d;
+                return (new Pair(
+                    new Pair((int)Math.Floor(half), input.Depth + 1),
+                    new Pair((int)Math.Ceiling(half), input.Depth + 1),
+                    input.Depth
+                    ), true);
+            }
+            return (input, false);
+        }
+
+        var (leftPair, leftUpdated) = Split(input.ValueLeft!);
+
+        if (leftUpdated)
+        {
+            return (input with { ValueLeft = leftPair }, true);
+        }
+
+        var (rightPair, rightUpdated) = Split(input.ValueRight!);
+
+        if (rightUpdated)
+        {
+            return (input with { ValueRight = rightPair }, true);
+        }
+
+        return (input, false);
+    }
+}
