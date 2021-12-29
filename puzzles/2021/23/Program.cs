7fbc49495fe3b0999d437e5fecccd6106cfc2f90
Anton Rademaker <info@antonrademaker.com>
2021-12-29 14:52:45 +0100

    Day 23, part 1

---
+++ puzzles/2021/23/Program.cs
@@ -0,0 +1,462 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Text;
+
+var inputFiles = new string[] {
+//    "input/example.txt",
+    "input/input.txt"
+};
+var sw = new Stopwatch();
+
+
+foreach (var exampleFile in inputFiles)
+{
+    Console.WriteLine(exampleFile);
+    var file = File.ReadAllLines(exampleFile);
+    sw.Start();
+
+    CalculatePart1(file);
+
+    sw.Stop();
+    Console.WriteLine($"Part 1 in {sw.ElapsedMilliseconds}ms");
+    sw.Reset();
+    sw.Start();
+
+    CalculatePart2(file);
+
+    sw.Stop();
+    Console.WriteLine($"Part 2 in {sw.ElapsedMilliseconds}ms");
+
+    Console.WriteLine("-- End of file--");
+}
+
+void CalculatePart1(string[] lines)
+{
+    var map = new Map();
+
+    var roomLine1 = lines[2].Split('#', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(s => s[0]).ToArray();
+    var roomLine2 = lines[3].Split('#', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(s => s[0]).ToArray();
+
+    var positions = ImmutableDictionary<int, Letter>.Empty;
+    var startPosition = 11;
+
+    for (var x = 0; x < roomLine2.Length; x++)
+    {
+        positions = positions.Add(startPosition++, LetterFactory(roomLine1[x]));
+        positions = positions.Add(startPosition++, LetterFactory(roomLine2[x]));
+    }
+
+    for (var y = 18; y > 10; y--)
+    {
+
+        if (map.Edges[y].Accepts == positions[y].Char && (y % 2 == 0 || positions[y + 1].IsFinished))
+        {
+            var cur = positions[y];
+            positions = positions.Remove(y).Add(y, cur with { IsFinished = true });
+        }
+
+    }
+
+    positions.Print();
+
+    var queue = new PriorityQueue<(ImmutableDictionary<int, Letter>, long), int>();
+
+    queue.Enqueue((positions, 0L), 0);
+
+    var currentLow = long.MaxValue;
+
+    var calculated = new HashSet<long>();
+
+    while (queue.TryDequeue(out var s, out var good))
+    {
+        (var state, var usedEnergy) = s;
+        var newStates = map.CalculateNewStates(state, usedEnergy);
+
+    //    Console.WriteLine($"Retrieved from queue: {usedEnergy} / {8-good}");
+
+//        state.Print();
+        
+  //          Console.WriteLine($"Generating new states:");
+
+        foreach (var newState in newStates)
+        {
+            if (newState.newState.All(t => t.Value.IsFinished))
+            {
+                currentLow = Math.Min(currentLow, usedEnergy + newState.energy);
+             //   Console.WriteLine($"Finished {currentLow} ({queue.Count})");
+            }
+            else
+            {
+                var newGood = newState.newState.Count(t => t.Value.IsFinished);
+                //Console.WriteLine($"New state: {newGood}, {usedEnergy + newState.energy}");
+
+                if (calculated.Add(CalculatePositionHash(newState.newState, usedEnergy + newState.energy)))
+                {
+
+      //              newState.newState.Print();
+
+                    queue.Enqueue((newState.newState, usedEnergy + newState.energy), 8 - newGood);
+                } else
+                {
+           //         Console.Write(".");
+                }
+            }
+        }
+        
+        //Console.WriteLine($"done queueing, new queue size: {queue.Count}");
+        // Console.ReadLine();
+    }
+
+    Console.WriteLine($"Lowest: {currentLow}");
+
+}
+
+long CalculatePositionHash(ImmutableDictionary<int, Letter> positions, long energy) {
+    var pHash = positions.Aggregate(1L, (acc, cur) => acc ^ (cur.Key * 997 * cur.Value.Char.GetHashCode() ) );
+
+    return pHash * (991 * energy);
+}
+{
+
+}
+
+void CalculatePart2(string[] lines)
+{
+}
+
+Letter LetterFactory(char letter)
+{
+    return letter switch
+    {
+        'A' => new Letter('A', 1),
+        'B' => new Letter('B', 10),
+        'C' => new Letter('C', 100),
+        _ => new Letter('D', 1000),
+    };
+}
+
+public record Letter(char Char, int Weight)
+{
+    public bool HasMoved { get; init; }
+    public bool IsFinished { get; init; }
+}
+
+public class Map
+{
+
+    public Map()
+    {
+        var cantStop = new List<int> { 2, 4, 6, 8 };
+
+        var position = 11;
+
+
+        Edges.AddRange(
+            Enumerable.Range(0, position).Select(i => new Edge
+            {
+                Id = i,
+                IsTopRow = true,
+                CantStop = cantStop.Contains(i)
+            }));
+
+
+        foreach (var c in "ABCD")
+        {
+            Edges.AddRange(Enumerable.Range(position, 2).Select(i => new Edge
+            {
+                Id = i,
+                Accepts = c
+            }));
+
+            position += 2;
+        }
+        Segments.Add(Edges[0], new List<Segment> {
+            new Segment(Edges[1])
+        });
+        for (var x = 1; x < 10; x++)
+        {
+
+            Segments.Add(Edges[x], new List<Segment> {
+                new Segment(Edges[x+1]),
+                new Segment(Edges[x-1])
+            });
+
+        }
+
+        Segments.Add(Edges[10], new List<Segment> {
+            new Segment(Edges[9])
+        });
+
+        var baseRow = 2;
+
+        var colStart = 11;
+
+        var colSize = 2;
+
+        for (int x = 0; x < 4; x++)
+        {
+
+            Segments.Add(Edges[colStart], new List<Segment> {
+                new Segment(Edges[colStart+1]),
+                new Segment(Edges[baseRow])}
+            );
+
+
+            Segments.Add(Edges[colStart + 1], new List<Segment> { new Segment(Edges[colStart]) });
+
+            Segments[Edges[baseRow]].Add(new Segment(Edges[colStart]));
+
+            colStart += colSize;
+            baseRow += colSize;
+        }
+    }
+
+    public List<Edge> Edges { get; set; } = new();
+
+    public Dictionary<Edge, List<Segment>> Segments { get; set; } = new();
+
+
+    public IEnumerable<(ImmutableDictionary<int, Letter> newState, long energy)> CalculateNewStates(ImmutableDictionary<int, Letter> state, long currentEnergy)
+    {
+        foreach (var (position, letter) in state.Where(t => !t.Value.IsFinished).OrderByDescending(t => t.Value.Weight))
+        {
+            foreach (var newPosition in GetReachablePositions(state, position, letter))
+            {
+                // move the letter
+                var newState = state.Remove(position).Add(newPosition.newPosition, newPosition.result);
+
+
+                yield return (newState, newPosition.newEnergy);
+
+            }
+        }
+    }
+
+    private IEnumerable<(int newPosition, long newEnergy, Letter result)> GetReachablePositions(ImmutableDictionary<int, Letter> state, int position, Letter letter)
+    {
+        var dict = new Dictionary<int, long>();
+
+        if (letter.IsFinished)
+        {
+            throw new Exception("Cannot move letter, it's already finished");
+        }
+
+        foreach (var reachable in GetReachablePositions(dict, state, position, letter, false))
+        {
+            if (!dict.ContainsKey(reachable.newPosition) || dict[reachable.newPosition] > reachable.newEnergy)
+            {
+                dict[reachable.newPosition] = reachable.newEnergy;
+            }
+        }
+
+        var resultLetter = letter;
+
+        if (!resultLetter.HasMoved)
+        {
+            resultLetter = resultLetter with
+            {
+                HasMoved = true
+            };
+        }
+        else
+        {
+            resultLetter = resultLetter with
+            {
+                IsFinished = true
+            };
+
+        }
+
+        foreach (var destination in dict)
+        {
+            yield return (destination.Key, destination.Value, resultLetter);
+        }
+
+    }
+
+    private IEnumerable<(int newPosition, long newEnergy)> GetReachablePositions(Dictionary<int, long> dict, ImmutableDictionary<int, Letter> state, int position, Letter letter, bool switched)
+    {
+        var positionsVisited = ImmutableHashSet<int>.Empty;
+        foreach (var reachable in ReachablePositions(positionsVisited, dict, state, position, position, letter, switched))
+        {
+            yield return reachable;
+        }
+
+    }
+
+    private IEnumerable<(int newPosition, long newEnergy)> ReachablePositions(ImmutableHashSet<int> positionsVisited, Dictionary<int, long> dict, ImmutableDictionary<int, Letter> state, int oldPosition, int position, Letter letter, bool switched)
+    {
+        var currentPosition = Edges[position];
+
+        if (position == 14)
+        {
+            //      Console.WriteLine(14);
+        }
+
+        foreach (var reachable in Segments[currentPosition])
+        {
+
+            var newPosition = reachable.Other.Id;
+
+            if (state.ContainsKey(newPosition))
+            {
+                continue;
+            }
+
+
+
+            if (positionsVisited.Contains(newPosition))
+            {
+                continue;
+            }
+
+            var path = positionsVisited.Add(newPosition);
+
+            var hasSwitched = switched;
+
+
+            if (switched)
+            {
+                //if (currentPosition.IsTopRow != Edges[newPosition].IsTopRow)
+                //{
+                //    continue;
+                //}
+
+            }
+            else
+            {
+                if (currentPosition.IsTopRow != Edges[newPosition].IsTopRow)
+                {
+                    hasSwitched = true;
+                }
+            }
+
+
+            var newEnergy = letter.Weight;
+
+            //if (!dict.ContainsKey(newPosition) || dict[newPosition] > newEnergy)
+            //{
+            if (AllowedMove(state, oldPosition, newPosition, letter, hasSwitched))
+            {
+                yield return (reachable.Other.Id, letter.Weight);
+            }
+
+            foreach (var steps in ReachablePositions(path, dict, state, oldPosition, newPosition, letter, hasSwitched))
+            {
+                var energy = letter.Weight + steps.newEnergy;
+
+
+                //if (!dict.ContainsKey(steps.newPosition) || dict[steps.newPosition] > steps.newEnergy)
+                //{
+
+
+                //   dict[steps.newPosition] = steps.newEnergy;
+
+                yield return (steps.newPosition, energy);
+                // }
+            }
+            //}
+        }
+    }
+
+
+    private bool AllowedMove(ImmutableDictionary<int, Letter> state, int position, int newPosition, Letter letter, bool hasSwitched)
+    {
+        if (!hasSwitched)
+        {
+            return false;
+        }
+
+        if (Edges[newPosition].CantStop)
+        {
+            return false;
+        }
+
+        if (!letter.HasMoved)
+        {
+            if (!Edges[newPosition].IsTopRow)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (Edges[newPosition].IsTopRow)
+            {
+                return false;
+            }
+            if (Edges[newPosition].Accepts != letter.Char)
+            {
+                return false;
+            }
+        }
+
+        if (newPosition >= 11 && (newPosition % 2) == 1)
+        {
+            if (!state.ContainsKey(newPosition + 1) || !state[newPosition + 1].IsFinished)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+}
+
+public struct Edge
+{
+    public int Id { get; init; }
+
+    public bool IsTopRow { get; init; }
+
+    public char? Accepts { get; init; }
+
+    public bool CantStop { get; init; }
+}
+
+public record Segment(Edge Other);
+
+
+public static class Helpers
+{
+
+    public static void Print(this ImmutableDictionary<int, Letter> positions)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine(new String('#', 13));
+
+        sb.Append('#');
+        for (var i = 0; i < 11; i++)
+        {
+            sb.Append(positions.TryGetValue(i, out var letter) ? letter.Char : '.');
+        }
+        sb.Append("#");
+        sb.AppendLine();
+        sb.Append("###");
+
+        var firstRow = 11;
+        var secondRow = 12;
+
+        for (var i = 0; i < 4; i++)
+        {
+            sb.Append(positions.TryGetValue((i * 2) + firstRow, out var letter) ? letter.Char : '.');
+            sb.Append('#');
+        }
+
+        sb.AppendLine("##");
+
+        sb.Append("  #");
+        for (var i = 0; i < 4; i++)
+        {
+            sb.Append(positions.TryGetValue((i * 2) + secondRow, out var letter) ? letter.Char : '.');
+            sb.Append('#');
+        }
+
+        sb.AppendLine();
+        sb.AppendLine($"  {new String('#', 9)}");
+
+        Console.WriteLine(sb.ToString());
+    }
+}
