7ff06a997775bfe180265f99a2b69eb29ffcafc4
Anton Rademaker <anton@freshblue.nl>
2022-12-01 07:45:20 +0100

    Day 1

---
+++ puzzles/2022/1/Program.cs
@@ -0,0 +1,11 @@
+﻿var calories = System.IO.File.ReadAllText("input/input.txt")
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(s => s.Split(Environment.NewLine)).Select(lines => lines.Select(line => int.Parse(line)).Sum()).ToList();
+
+var part1 = calories.Max();
+
+Console.WriteLine($"Part 1: {part1}");
+
+var part2 = calories.OrderByDescending(t => t).Take(3).Sum();
+
+Console.WriteLine($"Part 2: {part2}");
