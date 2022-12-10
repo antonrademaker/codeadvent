3bb154966d00e609fb55d5737093c3d5fc07110d
Anton Rademaker <antonrademaker@gmail.com>
2022-12-10 10:53:13 +0100

    Day 10

---
+++ puzzles/2022/10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+var files = Directory.GetFiles("input", "*.txt");
+
+foreach (var file in files.OrderBy(t => t))
+{
+    var lines = File.ReadAllLines(file);
+
+    var registerResults = Calculate(lines).ToList();
+
+    var positions = new List<int> { 20, 60, 100, 140, 180, 220 };
+
+    var locs = positions.Select(pos => new { value = registerResults[pos - 1], pos });
+
+    var sum = locs.Select(a => a.pos * a.value).Sum();
+    Console.WriteLine($"File: {file} Part 1: {sum}");
+
+    var sb = new StringBuilder();
+
+    for (var i = 0; i < 240; i++)
+    {
+        var register = registerResults[i];
+        var xScreen = i % 40;
+
+        sb.Append(register - 1 <= xScreen && xScreen <= register + 1 ? '#' : '.');
+
+        if (xScreen == 39)
+        {
+            sb.AppendLine();
+        }
+    }
+    Console.WriteLine(sb.ToString());
+}
+
+static IEnumerable<int> Calculate(string[] lines)
+{
+    var x = 1;
+    foreach (var line in lines)
+    {
+        yield return x;
+        if (!line.StartsWith("n"))
+        {
+            yield return x;
+            x += int.Parse(line[5..]);
+        }
+    }
+    yield return x;
+}
