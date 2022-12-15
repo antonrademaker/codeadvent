2187fd95bcb545b359a65e92b04f0fffd5d4275e
Anton Rademaker <anton@freshblue.nl>
2022-12-15 10:07:00 +0100

    Day 15 complete

--- puzzles/2022/15/Program.cs
+++ puzzles/2022/15/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 var files = Directory.GetFiles("input", "*.txt");
 
@@ -6,26 +7,87 @@ var regex = new Regex("^Sensor at x=(?<sensorX>-?\\d+), y=(?<sensorY>\\-?\\d+): 
 
 foreach (var file in files.OrderBy(t => t))
 {
-    var fileInput = File.ReadAllLines(file).Select(l => regex.Matches(l).Cast<Match>()
+    Console.WriteLine($"{file}");
+
+    var input = File.ReadAllLines(file).Select(l => regex.Matches(l).Cast<Match>()
     .Select(m => (sensor: new Point(int.Parse(m.Groups["sensorX"].Value), int.Parse(m.Groups["sensorY"].Value)),
     beacon: new Point(int.Parse(m.Groups["beaconX"].Value), int.Parse(m.Groups["beaconY"].Value)))).FirstOrDefault())
         .ToList();
 
-    var rowToCalculate = file.Contains("example") ? 10 : 2000000;
+    var part1 = CalculatePart1(input, file.Contains("example") ? 10 : 2000000);
 
-    Console.WriteLine($"Part 1: {Calculate(fileInput, rowToCalculate)}");
+    Console.WriteLine($"  Part 1: {part1}");
+
+    var part2 = CalculatePart2(input, file.Contains("example") ? 20 : 4000000);
+
+    Console.WriteLine($"  Part 2: {part2}");
 }
 
-static int Calculate(List<(Point sensor, Point beacon)> fileInput, int rowToCalculate)
+static int CalculatePart1(List<(Point sensor, Point beacon)> fileInput, int rowToCalculate)
 {
-    var hitsInRow = new HashSet<Point>();
-    var beacons = new HashSet<Point>();
+    var (ranges, beacons) = CalculateRow(fileInput, rowToCalculate);
+    return ranges.Sum(t => t.Length) - beacons.Count;
+}
 
-    foreach (var (sensor, beacon) in fileInput)
+static long CalculatePart2(List<(Point sensor, Point beacon)> fileInput, int size)
+{
+    for (var row = 0; row < size; row++)
+    {
+        var (ranges, beacons) = CalculateRow(fileInput, row);
+
+        var gaps = FindGaps(ranges, beacons, size);
+
+        if (gaps.Any())
+        {
+            return gaps.First() * 4000000L + row;
+        }
+    }
+    return -1;
+}
+
+static List<int> FindGaps(List<RowPart> ranges, HashSet<int> beacons, int size)
+{
+    List<int> results = new();
+    var rIndex = 0;
+
+    while (ranges[rIndex].End < 0 && rIndex < ranges.Count)
+    {
+        // skip all smaller ones
+        rIndex++;
+    }
+
+    var x = ranges[rIndex].End + 1;
+    rIndex++;
+
+    while (x <= size && rIndex < ranges.Count)
+    {
+        if (ranges[rIndex].Start != x)
+        {
+            if (!beacons.Contains(x))
+            {
+                results.Add(x);
+            }
+            x++;
+        }
+        else
+        {
+            x = ranges[rIndex].End;
+        }
+    }
+
+    return results;
+}
+
+static (List<RowPart> ranges, HashSet<int> beacons) CalculateRow(List<(Point sensor, Point beacon)> input, int rowToCalculate)
+{
+    var hitsInRow = new HashSet<RowPart>();
+    var beacons = new HashSet<int>();
+
+    foreach (var (sensor, beacon) in input)
     {
         if (beacon.Y == rowToCalculate)
         {
-            beacons.Add(beacon);
+            beacons.Add(beacon.X);
         }
 
         var distance = ManhattanDistance(sensor, beacon);
@@ -34,28 +96,55 @@ static int Calculate(List<(Point sensor, Point beacon)> fileInput, int rowToCalc
 
         if (toHitRowDistance < distance)
         {
-            hitsInRow.Add(new Point(sensor.X, rowToCalculate));
-            var steps = distance - toHitRowDistance;
-            for (var i = 1; i <= steps; i++)
+            //hitsInRow.Add(new Point(sensor.X, rowToCalculate));
+            var halfSize = distance - toHitRowDistance;
+
+            hitsInRow.Add(new RowPart(sensor.X - halfSize, sensor.X + halfSize));
+        }
+    }
+
+    // now reduce the ranges
+
+    var ranges = hitsInRow.OrderBy(t => t.Start).ThenBy(t => t.End).ToList();
+
+    var simpleRanges = new List<RowPart>
+    {
+        ranges[0]
+    };
+
+    for (var i = 1; i < ranges.Count; i++)
+    {
+        if (simpleRanges[^1].Start <= ranges[i].Start && simpleRanges[^1].End >= ranges[i].End)
+        {
+            continue;
+        }
+        else
+        {
+            if (simpleRanges[^1].End >= ranges[i].Start && simpleRanges[^1].End < ranges[i].End)
             {
-                hitsInRow.Add(new Point(sensor.X - i, rowToCalculate));
-                hitsInRow.Add(new Point(sensor.X + i, rowToCalculate));
+                simpleRanges[^1] = simpleRanges[^1] with { End = ranges[i].End };
+            }
+            else
+            {
+                Debug.Assert(simpleRanges[^1].End < ranges[i].Start);
+                simpleRanges.Add(ranges[i]);
             }
         }
     }
 
-    var min = hitsInRow.Min(t => t.X);
-    var max = hitsInRow.Max(t => t.X);
-
-    Console.WriteLine($"Min: {min}-{max}: hits: {hitsInRow.Count}: beacons: {beacons.Count}");
-
-    return hitsInRow.Count - beacons.Count;
+    return (simpleRanges, beacons);
 }
 
 static int ManhattanDistance(Point a, Point b)
 {
     return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
 }
+
+record struct RowPart(int Start, int End)
+{
+    public int Length => End - Start + 1;
+}
+
 record struct Point(int X, int Y)
 {
     public static Point operator +(Point a, Point b) => a with { X = a.X + b.X, Y = a.Y + b.Y };
