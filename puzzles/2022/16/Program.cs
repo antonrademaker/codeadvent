847d21e3828b7f8a1f6f7a51181d81760527454c
Anton Rademaker <antonrademaker@gmail.com>
2022-12-16 23:27:48 +0100

    Day 16

--- puzzles/2022/16/Program.cs
+++ puzzles/2022/16/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Immutable;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 var files = Directory.GetFiles("input", "*.txt");
 
 var regex = new Regex("^Valve (?<name>\\w\\w) has flow rate=(?<rate>\\d+); tunnel[s]? lead[s]? to valve[s]? (?<tunnels>((\\w\\w(, ){0,1})+))$", RegexOptions.NonBacktracking);
 
-foreach (var file in files.OrderBy(t => t).Take(1))
+foreach (var file in files.OrderBy(t => t))
 {
     Console.WriteLine($"==============={file}==============");
 
@@ -13,13 +14,15 @@ foreach (var file in files.OrderBy(t => t).Take(1))
     .Select(m => new Valve(m.Groups["name"].Value, int.Parse(m.Groups["rate"].Value), m.Groups["tunnels"].Value.Split(',', StringSplitOptions.TrimEntries).ToImmutableArray()))
         .ToDictionary(k => k.Name, k => k);
 
-    var part1 = CalculatePart1(input);
+    var edges = CalculateEdges(input);
+
+    var part1 = CalculatePart1(input, edges);
 
     Console.WriteLine($"  Part 1: {part1}");
 
-    //var part2 = CalculatePart2(input, file.Contains("example") ? 20 : 4000000);
+    var part2 = CalculatePart2(input, edges);
 
-    //Console.WriteLine($"  Part 2: {part2}");
+    Console.WriteLine($"  Part 2: {part2}");
 }
 
 static IList<(string source, string target, int weight, int rate)> GetEdges(Dictionary<string, Valve> valves, string source, int currentWeight, string target, HashSet<string> visited)
@@ -27,134 +30,140 @@ static IList<(string source, string target, int weight, int rate)> GetEdges(Dict
     var valve = valves[target];
 
     var results = new List<(string source, string target, int weight, int rate)>();
-
-    if (valve.Rate == 0)
+    foreach (var nextValve in valve.Tunnels.Where(v => !visited.Contains(v)))
     {
-        Console.WriteLine($"  skipping {target} (rate: {valve.Rate})");
-        foreach (var nextValve in valve.Tunnels.Where(v => !visited.Contains(v)))
+        visited.Add(nextValve);
+
+        results.AddRange(GetEdges(valves, source, currentWeight + 1, nextValve, visited));
+        if (valve.Rate > 0)
         {
-            visited.Add(nextValve);
-
-            results.AddRange(GetEdges(valves, source, currentWeight + 1, nextValve, visited));
+            results.Add((source, target, currentWeight, valve.Rate));
         }
-    }
-    else
-    {
-        Console.WriteLine($"  target {target} (rate: {valve.Rate})");
-
-        results.Add((source, target, currentWeight, valve.Rate));
     }
     return results;
 }
 
-
-static Dictionary<string, Dictionary<string, (int weight, int rate)>> CalculateEdges(Dictionary<string, Valve> valves)
+static Dictionary<string, (int weight, int rate)> CalculateDistances(Dictionary<string, Valve> valves, string start)
 {
-    var edges = new Dictionary<string, Dictionary<string, (int weight, int rate)>>();
+    var distances = new Dictionary<string, (int weight, int rate)>();
 
-    var vQueue = new Queue<string>();
-    vQueue.Enqueue("AA");
+    var vQueue = new PriorityQueue<string, int>();
+    vQueue.Enqueue(start, 0);
 
-    var visited = new HashSet<string>() { "AA" };
+    var visited = new HashSet<string>();
 
-    while (vQueue.TryDequeue(out var valveId))
+    while (vQueue.TryDequeue(out var valve, out var prio))
     {
-        var valve = valves[valveId];
-
-
-        var results = new List<(string source, string target, int weight, int rate)>();
-
-        foreach (var tunnel in valve.Tunnels)
+        var valveData = valves[valve];
+        if (valveData.Rate > 0 && (!distances.TryGetValue(valve, out var data) || data.weight > prio))
         {
-            Console.WriteLine($"Checking {valveId} => {tunnel} ");
-            var calculatedEdges = GetEdges(valves, valveId, 1, tunnel, new HashSet<string>() { valveId });
+            distances[valve] = (prio, valveData.Rate);
+        }
 
-            foreach (var calc in calculatedEdges)
+        foreach (var tunnel in valveData.Tunnels)
+        {
+            if (!visited.Contains(tunnel))
             {
-                if (!edges.TryGetValue(calc.source, out var targets))
-                {
-                    targets = new Dictionary<string, (int weight, int rate)>();
-                }
-
-                if (!targets.TryGetValue(calc.source, out var data) || data.weight > calc.weight)
-                {
-                    Console.WriteLine($"Adding {valveId} => {calc.target} ({calc.weight})");
-
-                    targets[calc.source] = (calc.weight, calc.rate);
-
-                    if (!visited.Contains(calc.target))
-                    {
-                        Console.WriteLine($"Queue {calc.target}");
-
-                        vQueue.Enqueue(calc.target);
-                        visited.Add(calc.target);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Not queueing {calc.target}");
-
-                    }
-                }
-
-                edges[calc.source] = targets;
+                vQueue.Enqueue(tunnel, prio + 1);
+                visited.Add(tunnel);
             }
         }
     }
 
-    return edges;
+    return distances;
 }
 
-static int CalculatePart1(Dictionary<string, Valve> valves)
+static Dictionary<string, Dictionary<string, (int weight, int rate)>> CalculateEdges(Dictionary<string, Valve> valves)
 {
-    var edges = CalculateEdges(valves);
+    return valves.Keys.Select(valve => (valve, distances: CalculateDistances(valves, valve))).ToDictionary(k => k.valve, k => k.distances);
+}
 
-    //var startPath = new List<string>() { "AA" }.ToImmutableArray();
-    //var open = new List<string>().ToImmutableArray();
-
-    //var queue = new Queue<(ImmutableArray<string> path, ImmutableArray<string> open, int timeLeft, int value)>();
-
+static int CalculatePart1(Dictionary<string, Valve> valves, Dictionary<string, Dictionary<string, (int weight, int rate)>> edges)
+{
     var openableValves = valves.Values.Where(t => t.Rate > 0).Select(t => t.Name).ToImmutableHashSet();
-
 
     var best = CalculateBest(edges, openableValves);
 
-
     return best;
 }
 
-static int CalculateBest(Dictionary<string, Dictionary<string, (int weight, int rate)>> edges, ImmutableHashSet<string> openableValves, string current = "AA", int timeLeft = 30, int currentBest)
+static int CalculatePart2(Dictionary<string, Valve> valves, Dictionary<string, Dictionary<string, (int weight, int rate)>> edges)
 {
-    if (openableValves.Count == 0)
+    var openableValves = valves.Values.Where(t => t.Rate > 0).Select(t => t.Name).ToImmutableHashSet();
+
+    var best = CalculateBestWithElephant(edges, openableValves);
+
+    return best;
+}
+
+static int CalculateBest(Dictionary<string, Dictionary<string, (int weight, int rate)>> edges, ImmutableHashSet<string> openableValves, string current = "AA", int timeLeft = 30, int currentPressure = 0)
+{
+    if (openableValves.Count == 0 || timeLeft < 0)
     {
-        return currentBest;
+        return currentPressure;
     }
 
-    var best = currentBest;
-
-
-    foreach (var edge in edges[current])
+    var best = currentPressure;
+    foreach (var edge in edges[current].Where(edge => openableValves.Contains(edge.Key)))
     {
-
+        var newTimeLeft = timeLeft - edge.Value.weight - 1;
+        var candidateToOpen = CalculateBest(edges, openableValves.Remove(edge.Key), edge.Key, newTimeLeft, currentPressure + (edge.Value.rate * newTimeLeft));
+        if (candidateToOpen > best)
+        {
+            best = candidateToOpen;
+        }
     }
 
     return best;
 }
 
-//static long CalculatePart2(List<(Point sensor, Point beacon)> fileInput, int size)
-//{
-//    for (var row = 0; row < size; row++)
-//    {
-//        var (ranges, beacons) = CalculateRow(fileInput, row);
 
-//        var gaps = FindGaps(ranges, beacons, size);
+static int CalculateBestWithElephant(Dictionary<string, Dictionary<string, (int weight, int rate)>> edges, ImmutableHashSet<string> openableValves, string current1 = "AA", string current2 = "AA", int timeLeft1 = 26, int timeLeft2 = 26, int currentPressure = 0)
+{
+    if (openableValves.Count == 0 || timeLeft1 <= 0 || timeLeft2 <= 0)
+    {
+        return currentPressure;
+    }
 
-//        if (gaps.Any())
-//        {
-//            return gaps.First() * 4000000L + row;
-//        }
-//    }
-//    return -1;
-//}
+    var best = currentPressure;
+    foreach (var newCurrent1 in openableValves)
+    {
+        var edge = edges[current1][newCurrent1];
+
+        var newTimeLeft1 = timeLeft1 - edge.weight - 1;
+
+        if (newTimeLeft1 > 0)
+        {
+            var openableValvesNext1 = openableValves.Remove(newCurrent1);
+
+            var newPressure1 = currentPressure + (edge.rate * newTimeLeft1);
+
+            foreach (var edgeCode2 in openableValvesNext1)
+            {
+                var (weight, rate) = edges[current2][edgeCode2];
+                var newTimeLeft2 = timeLeft2 - weight - 1;
+                if (newTimeLeft2 > 0)
+                {
+                    var openableValvesNext2 = openableValvesNext1.Remove(edgeCode2);
+                    var newPressure2 = newPressure1 + (rate * newTimeLeft2);
+
+                    var candidateToOpen = CalculateBestWithElephant(edges, openableValvesNext2, newCurrent1, edgeCode2, newTimeLeft1, newTimeLeft2, newPressure2);
+                    if (candidateToOpen > best)
+                    {
+                        best = candidateToOpen;
+                    }
+                }
+            }
+
+            var bestAlone = CalculateBestWithElephant(edges, openableValvesNext1, newCurrent1, current2, newTimeLeft1, timeLeft2, newPressure1);
+            if (bestAlone > best)
+            {
+                best = bestAlone;
+            }
+        }
+    }
+    return best;
+}
 
 public record Valve(string Name, int Rate, ImmutableArray<string> Tunnels)
 {
