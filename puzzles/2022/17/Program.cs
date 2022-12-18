e0107595f29757bf8045bc252c25b3f492bf335f
Anton Rademaker <antonrademaker@gmail.com>
2022-12-18 22:55:09 +0100

    Day 17 - part 2

--- puzzles/2022/17/Program.cs
+++ puzzles/2022/17/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Text;
+﻿using System.Text;
+
+namespace AdventOfCode.Day17;
 
 internal class Program
 {
@@ -8,20 +8,16 @@ internal class Program
     {
         var files = Directory.GetFiles("input", "*.txt");
 
-        foreach (var file in files.OrderBy(t => t).Take(1))
+        foreach (var file in files.OrderBy(t => t))
         {
             Console.WriteLine($"==============={file}==============");
 
             var input = File.ReadAllText(file).Trim();
 
-            var (part1,part2) = Calculate(input);
+            var (part1, part2) = Calculate(input);
 
             Console.WriteLine($"  Part 1: {part1}");
             Console.WriteLine($"  Part 2: {part2}");
-
-            //var part2 = CalculatePart2(input, edges);
-
-            //Console.WriteLine($"  Part 2: {part2}");
         }
     }
 
@@ -32,24 +28,20 @@ internal class Program
 
         var left = new Point(-1, 0);
         var right = new Point(1, 0);
+        var downMovement = new Point(0, -1);
 
         var currentHeight = 0;
 
         var rocksFallen = 0L;
 
-       // var currentRocks = new List<IShape>();
-
-        var downMovement = new Point(0, -1);
         var nextRockShape = -1;
 
         var part1 = 0L;
         var part2 = 0L;
 
         var part1Index = 2022L;
-        var part2Index = 22220L; // 1000000000000L;
 
         var tower = new HashSet<Point>();
-
 
         var cache = new Dictionary<(int rockIndex, int patternIndex), (long rocks, long height)>();
 
@@ -60,7 +52,6 @@ internal class Program
             // jet movement
             while (true)
             {
-
                 var jet = input[patternIndex] == '<' ? left : right;
 
                 patternIndex = (patternIndex + 1) % patternLenght;
@@ -80,47 +71,43 @@ internal class Program
                 }
                 else
                 {
-                    //var restingRock = new RestingRock(movingRock);
-
-                    foreach(var p in movingRock.Points)
-                    {
-                        Debug.Assert(tower.Add(p));
-                    }
-                    //currentRocks.Add(restingRock);
                     var newHeight = movingRock.MaxHeight;
                     if (newHeight > currentHeight)
                     {
                         currentHeight = newHeight;
                     }
+
+                    foreach (var p in movingRock.Points)
+                    {
+                        tower.Add(p);
+                    }
                     break;
                 }
             }
 
+            rocksFallen++;
+
             if (cache.TryGetValue((nextRockShape, patternIndex), out var hit) && part2 == 0)
             {
-                // Console.WriteLine("Got a cache hit!!!");
-
                 long cycle = rocksFallen - hit.rocks;
-                long adds = currentHeight - hit.height;
-                long remaining = 1000000000000 - rocksFallen;
-                long combo = (remaining / (cycle) + 1);
-                if (rocksFallen + combo * cycle >= 1000000000000)
-                {
-                    if (rocksFallen + combo * cycle > 1000000000000)
-                    {
-                        Console.WriteLine("We missed the spot");
-                    }
-                    else
-                    {
-                        part2 = currentHeight + 1 + combo * adds;
-                    }
-                }
-            } else
-            {
-                cache[(nextRockShape, patternIndex)] = (rocksFallen, currentHeight + 1);
-            }
+                long heightDifference = currentHeight - hit.height;
+                long remaining = 1000000000000 - rocksFallen - 1;
+                long combo = remaining / cycle + 1;
 
-            rocksFallen++;
+                var test = rocksFallen + (combo * cycle);
+
+                if (test == 1000000000000)
+                {
+                    part2 = currentHeight + (combo * heightDifference);
+                }
+            }
+            else
+            {
+                if (rocksFallen > 1500)
+                {
+                    cache[(nextRockShape, patternIndex)] = (rocksFallen, currentHeight);
+                }
+            }
 
             if (rocksFallen == part1Index)
             {
@@ -145,7 +132,6 @@ internal class Program
         };
     }
 
-
     private static void Print(string message, HashSet<Point> tower, int currentHeight)
     {
         var y = currentHeight;
@@ -154,73 +140,6 @@ internal class Program
         var sb = new StringBuilder();
 
         var allPoints = tower.OrderByDescending(t => t.Y).ThenBy(t => t.X).ToList();
-
-
-        sb.AppendLine(message);
-
-        foreach (var point in allPoints)
-        {
-            while (y > point.Y)
-            {
-                y--;
-                if (x == 0)
-                {
-                    sb.AppendLine("|.......|");
-                    continue;
-                }
-                if (x < 8)
-                {
-                    sb.Append(new String('.', 8 - x));
-                }
-
-                sb.Append('|');
-                x = 0;
-                sb.AppendLine();
-            }
-
-            if (x == 0)
-            {
-                sb.Append('|');
-                x++;
-            }
-            while (point.X > x)
-            {
-                sb.Append('.');
-                x++;
-            }
-            sb.Append('#');
-            x++;
-        }
-        y--;
-
-        if (x < 9)
-        {
-            sb.Append(new String('.', 8 - x));
-            sb.Append("|");
-        }
-
-        sb.AppendLine();
-        while (y > 0)
-        {
-            y--;
-            sb.AppendLine("|.......|");
-        }
-
-        sb.AppendLine(new string('-', 9));
-        sb.AppendLine();
-        Console.WriteLine(sb.ToString());
-    }
-
-
-    private static void Print(string message, List<IShape> shapes)
-    {
-        var allPoints = shapes.SelectMany(s => s.Points).OrderByDescending(t => t.Y).ThenBy(t => t.X).ToList();
-
-        var y = allPoints[0].Y;
-
-        var x = 0;
-
-        var sb = new StringBuilder();
 
         sb.AppendLine(message);
 
@@ -290,48 +209,21 @@ public interface IShape
 
     HashSet<Point> Points { get; }
 
-    bool HitsLeftWall { get; } // => Points.Any(t => t.X == 0);
-    bool HitsRightWall { get; } // => Points.Any(t => t.X == 8);
-    bool HitsBottom { get; } //=> Points.Any(t => t.Y == 0);
+    bool HitsLeftWall { get; }
+    bool HitsRightWall { get; }
+    bool HitsBottom => Point.Y == 0;
 
     bool HitsWall => HitsLeftWall || HitsRightWall;
 
     IShape Move(Point move);
 }
 
-public record struct RestingRock : IShape
-{
-    public Point Point { get; }
-
-    public int MaxHeight { get; }
-
-    public HashSet<Point> Points { get; }
-
-    public bool HitsLeftWall => throw new NotImplementedException();
-
-    public bool HitsRightWall => throw new NotImplementedException();
-
-    public bool HitsBottom => throw new NotImplementedException();
-
-    public RestingRock(IShape shape)
-    {
-        Point = shape.Point;
-        Points = shape.Points;
-        MaxHeight = shape.MaxHeight;
-    }
-
-    public IShape Move(Point move)
-    {
-        throw new NotImplementedException();
-    }
-}
-
 public record struct Minus(Point Point) : IShape
 {
     static readonly HashSet<Point> points = new()
-    {
-        new Point(0,0), new Point(1,0), new Point(2,0), new Point(3,0)
-    };
+        {
+            new Point(0,0), new Point(1,0), new Point(2,0), new Point(3,0)
+        };
 
     public HashSet<Point> Points
     {
@@ -352,8 +244,6 @@ public record struct Minus(Point Point) : IShape
     public bool HitsLeftWall => Point.X == 0;
 
     public bool HitsRightWall => Point.X == 5;
-
-    public bool HitsBottom => Point.Y == 0;
 }
 
 public record struct Plus(Point Point) : IShape
@@ -382,8 +272,6 @@ public record struct Plus(Point Point) : IShape
 
     public bool HitsLeftWall => Point.X == 0;
     public bool HitsRightWall => Point.X == 6;
-
-    public bool HitsBottom => Point.Y == 0;
 }
 
 public record struct InvertedL(Point Point) : IShape
@@ -413,8 +301,6 @@ public record struct InvertedL(Point Point) : IShape
     public bool HitsLeftWall => Point.X == 0;
 
     public bool HitsRightWall => Point.X == 6;
-
-    public bool HitsBottom => Point.Y == 0;
 }
 
 public record struct Pipe(Point Point) : IShape
@@ -444,8 +330,6 @@ public record struct Pipe(Point Point) : IShape
     public bool HitsLeftWall => Point.X == 0;
 
     public bool HitsRightWall => Point.X == 8;
-
-    public bool HitsBottom => Point.Y == 0;
 }
 
 public record struct Block(Point Point) : IShape
@@ -473,8 +357,6 @@ public record struct Block(Point Point) : IShape
     public bool HitsLeftWall => Point.X == 0;
 
     public bool HitsRightWall => Point.X == 7;
-
-    public bool HitsBottom => Point.Y == 0;
 }
 
 public record struct Point(int X, int Y)
