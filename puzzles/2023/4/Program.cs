66ba9292f7c586c26501a90dee51f22b05d1ba90
Anton Rademaker <antonrademaker@gmail.com>
2023-12-30 16:49:09 +0100

    Day 4 - part 2

--- puzzles/2023/4/Program.cs
+++ puzzles/2023/4/Program.cs
@@ -9,17 +9,18 @@ foreach (var file in inputFiles)
 
     var inputs = System.IO.File.ReadAllLines(file);
 
-    var scratchPoints = inputs.Select(input => CalculateScratchPoints(input));
+    var scratchPoints = inputs.Select(CalculateScratchPoints);
 
-    var part1 = scratchPoints.Sum();
+    var part1 = scratchPoints.Select(t => t.score).Sum();
 
     Console.WriteLine($"Part 1: {part1}");
 
-    //   var part2 = gears.Sum();
-    //   Console.WriteLine($"Part 2: {part2}");
+    var part2 = CalculateTotalNumberOfCards(inputs);
+
+    Console.WriteLine($"Part 2: {part2}");
 }
 
-int CalculateScratchPoints(string input)
+(int cardId, int score, int matches) CalculateScratchPoints(string input)
 {
     Console.WriteLine(input);
 
@@ -39,6 +40,32 @@ int CalculateScratchPoints(string input)
     var score = matches > 0 ? Enumerable.Range(1, matches - 1).Aggregate(1, (cur, next) => cur * 2) : 0;
     Console.WriteLine($"\tWinning: {string.Join(",", winningScratchcards)}, points: {score}");
 
-    return score;
+    return (cardId, score, matches);
+}
 
+int CalculateTotalNumberOfCards(string[] inputs)
+{
+    var cards = inputs.Select(CalculateScratchPoints).ToDictionary(inp => inp.cardId, inp => inp.matches);
+
+    var cardStack = cards.Keys.ToList();
+
+    var totalCards = 0;
+
+    while (cardStack.Any())
+    {
+        var currentCard = cardStack[0];
+        cardStack.RemoveAt(0);
+        totalCards++;
+
+        var toAdd = cards[currentCard];
+
+        var cardsToAdd = Enumerable.Range(currentCard + 1, toAdd).Reverse().ToList();
+
+        foreach (var card in cardsToAdd)
+        {
+            cardStack.Insert(0, card);
+        }
+    }
+
+    return totalCards;
 }
