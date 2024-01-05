a85fbb317fa064f0a3913b60b523c9e62f01bb68
Anton Rademaker <antonrademaker@gmail.com>
2024-01-05 12:03:40 +0100

    Day 10

--- puzzles/2023/10/Program.cs
+++ puzzles/2023/10/Program.cs
@@ -1,108 +1,162 @@
 ﻿using System.Diagnostics;
 
-string[] inputFiles = ["input/example.txt", "input/example1.txt","input/input.txt"];
+namespace AoC.Puzzles.Y2023.D10;
 
-foreach (var file in inputFiles)
+public static class Program
 {
-    Console.WriteLine(file);
+    private static readonly char[] oddEven = ['|', 'J', 'L'];
 
-    var inputs = System.IO.File.ReadAllLines(file);
 
-    var inputRows = inputs.SelectMany((input, y) => input.Select((c, x) => (x, y, location: new Location(c)))).ToDictionary(t => (t.x, t.y), t => t.location);
 
-    var part1 = CalculatePart1(inputRows);
-
-    Console.WriteLine($"Part 1: {part1}");
-
-    //var part2 = CalculatePart2(inputRows);
-
-    //Console.WriteLine($"Part 2: {part2}");
-}
-
-long CalculatePart1(Dictionary<(int x, int y), Location> inputs)
-{
-    var startX = 0;
-    var startY = 0;
-
-    foreach (var loc in inputs.Keys)
+    private static void Main(string[] args)
     {
-        if (inputs[loc].IsStart)
+        string[] inputFiles = ["input/example.txt", "input/example_a.txt", "input/example1.txt", "input/example2.txt", "input/input.txt"];
+
+        foreach (var file in inputFiles)
         {
-            startX = loc.x;
-            startY = loc.y;
+            Console.WriteLine(file);
 
+            var inputs = System.IO.File.ReadAllLines(file);
+
+            var inputRows = inputs.SelectMany((input, y) => input.Select((c, x) => (x, y, location: new Location(c)))).ToDictionary(t => (t.x, t.y), t => t.location);
+
+            var part1 = CalculatePart1(inputRows);
+
+            Console.WriteLine($"Part 1: {part1}");
+
+            var part2 = CalculatePart2(inputRows);
+
+            Console.WriteLine($"Part 2: {part2}");
         }
     }
 
-    var distances = new Dictionary<(int x, int y), int>();
-
-    var locations = new Queue<(int x, int y, Direction Direction, int depth)>();
-
-    AddLocationToQueue(locations, startX, startY, -1, 0);
-    AddLocationToQueue(locations, startX, startY, 1, 0);
-    AddLocationToQueue(locations, startX, startY, 0, -1);
-    AddLocationToQueue(locations, startX, startY, 0, 1);
-
-    while (locations.TryDequeue(out var location))
+    private static long CalculatePart1(Dictionary<(int x, int y), Location> inputs)
     {
-        if (inputs.TryGetValue((location.x, location.y), out var nextCandidate)) {
+        Dictionary<(int x, int y), int> distances = CalculateLoop(inputs);
 
-            Console.Write(nextCandidate.Character);
-
-            if (nextCandidate.IsStart)
-            {                
-                continue;
-            }
-
-            if (!distances.TryGetValue(((location.x, location.y)), out var dist) || dist > location.depth)
-            {
-                distances[(location.x, location.y)] = location.depth;
-            }
-
-            if (nextCandidate.First == location.Direction)
-            {
-                locations.Enqueue((location.x - nextCandidate.Second.X, location.y - nextCandidate.Second.Y, new Direction(-nextCandidate.Second.X, -nextCandidate.Second.Y), location.depth + 1));
-            }
-            else if (nextCandidate.Second == location.Direction)
-            {
-                locations.Enqueue((location.x - nextCandidate.First.X, location.y - nextCandidate.First.Y, new Direction(-nextCandidate.First.X, -nextCandidate.First.Y), location.depth + 1));
-            }
-        }
+        return distances.Values.Max();
     }
 
-    return distances.Values.Max();
-}
+    private static long CalculatePart2(Dictionary<(int x, int y), Location> inputs)
+    {
+        Dictionary<(int x, int y), int> distances = CalculateLoop(inputs);
 
-void AddLocationToQueue(Queue<(int x, int y, Direction dir, int depth)> queue, int startX, int startY, int xDiff, int yDiff)
-{
-    queue.Enqueue((startX + xDiff, startY + yDiff, new Direction(xDiff, yDiff), 1));
+        var maxY = inputs.Keys.Max(t => t.y);
+        var maxX = inputs.Keys.Max(t => t.x);
+        var enclosed = 0;
+
+        for (var y = 0; y <= maxY; y++)
+        {
+            var crossings = 0;
+            for (var x = 0; x <= maxX; x++)
+            {
+                var loc = inputs[(x, y)];
+                if (loc.IsStart)
+                {
+                    var neighbors = Direction.AllDirections.Select(dir => (dir, x: x + dir.X, y: y + dir.Y)).Where(val => distances.ContainsKey((val.x, val.y))).Select(val => val.dir).ToArray();
+
+                    var character = CharMapper.First(l => l.Value[0] == -neighbors[0] && l.Value[1] == -neighbors[1] || l.Value[1] == -neighbors[0] && l.Value[0] == -neighbors[1]);
+
+                    loc = loc with
+                    {
+                        Character = character.Key
+                    };
+                }
+                var partOfLoop = distances.ContainsKey((x, y));
+
+                if (partOfLoop)
+                {
+                    if (oddEven.Contains(loc.Character))
+                    {
+                        crossings++;
+                    }
+                }
+                else if (crossings % 2 == 1)
+                {
+                    enclosed++;
+                }
+            }
+        }
+
+        return enclosed;
+    }
+
+    private static Dictionary<(int x, int y), int> CalculateLoop(Dictionary<(int x, int y), Location> inputs)
+    {
+        var (startX, startY) = inputs.First(t => t.Value.IsStart).Key;
+
+        var distances = new Dictionary<(int x, int y), int>();
+
+        distances[(startX, startY)] = 0;
+
+        var locations = new Queue<(int x, int y, Direction Direction, int depth)>();
+
+        foreach (var direction in Direction.AllDirections)
+        {
+            locations.Enqueue((startX + direction.X, startY + direction.Y, direction, 1));
+        }
+
+        while (locations.TryDequeue(out var location))
+        {
+            if (inputs.TryGetValue((location.x, location.y), out var nextCandidate))
+            {
+                if (nextCandidate.IsStart)
+                {
+                    continue;
+                }
+
+                for (var i = 0; i < nextCandidate.Directions.Length; i++)
+                {
+                    if (nextCandidate.Directions[i] == location.Direction)
+                    {
+                        if (!distances.TryGetValue(((location.x, location.y)), out var dist) || dist > location.depth)
+                        {
+                            distances[(location.x, location.y)] = location.depth;
+                        }
+                        locations.Enqueue((location.x - nextCandidate.Directions[1 - i].X, location.y - nextCandidate.Directions[1 - i].Y, -nextCandidate.Directions[1 - i], location.depth + 1));
+                    }
+                }
+            }
+        }
+
+        return distances;
+    }
+
+    public static readonly IReadOnlyDictionary<char, Direction[]> CharMapper = new Dictionary<char, Direction[]>
+    {
+         { '|' , [Direction.Up, Direction.Down] },
+         {   '-' , [Direction.Left, Direction.Right] },
+         {   'L' , [Direction.Down, Direction.Left] },
+         {   'J' , [Direction.Down, Direction.Right]},
+         {   '7' , [Direction.Right, Direction.Up]},
+         {   'F' , [Direction.Left, Direction.Up]},
+         {   '.' , [new Direction(1, 1), new Direction(1, 1)]},
+         {   'S' , [new Direction(0, 0), new Direction(0, 0)]},
+    };
 }
 
 readonly record struct Location
 {
-    public readonly char Character { get; }
-    public readonly Direction First { get; }
-    public readonly Direction Second { get; }
+    public readonly char Character { get; init; }
+    public readonly Direction[] Directions { get; }
     public readonly bool IsStart { get; }
 
     public Location(char Location)
     {
+        IsStart = Location == 'S';
         Character = Location;
-        (First, Second) = Location switch
-        {
-            '|' => (new Direction(0, -1), new Direction(0, 1)),
-            '-' => (new Direction(-1, 0), new Direction(1, 0)),
-            'L' => (new Direction(0, 1), new Direction(-1, 0)),
-            'J' => (new Direction(0, 1), new Direction(1, 0)),
-            '7' => (new Direction(1, 0), new Direction(0, -1)),
-            'F' => (new Direction(-1, 0), new Direction(0, -1)),
-            '.' => (new Direction(1, 1), new Direction(1, 1)),
-            'S' => (new Direction(0, 0), new Direction(0, 0)),
-            _ => throw new ArgumentException("Invalid location", nameof(Location))
-        };
-
-        IsStart = First == Second && First == new Direction(0, 0);
+        Directions = Program.CharMapper[Location];
     }
 }
 
-readonly record struct Direction(int X, int Y);
+public readonly record struct Direction(int X, int Y)
+{
+    public static readonly Direction Up = new(0, -1);
+    public static readonly Direction Down = new(0, 1);
+    public static readonly Direction Left = new(-1, 0);
+    public static readonly Direction Right = new(1, 0);
+
+    public static readonly Direction[] AllDirections = [Up, Down, Left, Right];
+
+    public static Direction operator -(Direction direction) => new(-direction.X, -direction.Y);
+}
