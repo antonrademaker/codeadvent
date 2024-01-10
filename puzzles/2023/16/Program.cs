470c20ea5450cff2119e76d5b778cf6b1b9ffb91
Anton Rademaker <antonrademaker@gmail.com>
2024-01-10 16:54:48 +0100

    Day 16

---
+++ puzzles/2023/16/Program.cs
@@ -0,0 +1,211 @@
+﻿using AoC.Utilities;
+using System.Runtime.InteropServices;
+using System.Timers;
+using CoordinateDirection = (AoC.Utilities.Coordinate Position, AoC.Utilities.Coordinate Direction);
+
+namespace AoC.Puzzles.Y2023.D16;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] inputFiles = [
+            "input/example.txt"
+            , "input/input.txt"
+            ];
+
+        foreach (var file in inputFiles)
+        {
+            Console.WriteLine(file);
+
+            var inputs = System.IO.File.ReadAllLines(file);
+
+            var part1 = CalculatePart1(inputs);
+
+            Console.WriteLine($"Part 1: {part1}");
+
+            var part2 = CalculatePart2(inputs);
+
+            Console.WriteLine($"Part 2: {part2}");
+        }
+    }
+
+    private static long CalculatePart1(string[] input)
+    {
+        var (width, height, parsed) = ParseInputs(input);
+
+        var currentPosition = Coordinate.OffsetLeft;
+
+        var currentDirection = Coordinate.OffsetRight;
+
+        var energized = CalculateBeam(parsed, width, height, currentPosition, currentDirection);
+        return energized.Count;
+    }
+
+    private static long CalculatePart2(string[] input)
+    {
+        var (width, height, parsed) = ParseInputs(input);
+
+        var beams = new List<CoordinateDirection>();
+
+        beams.AddRange(Enumerable.Range(0, width).Select(x => new CoordinateDirection(new Coordinate(x,-1), Coordinate.OffsetDown)));
+        beams.AddRange(Enumerable.Range(0, width).Select(x => new CoordinateDirection(new Coordinate(x,height), Coordinate.OffsetUp)));
+        beams.AddRange(Enumerable.Range(0, height).Select(y => new CoordinateDirection(new Coordinate(-1,y), Coordinate.OffsetRight)));
+        beams.AddRange(Enumerable.Range(0, height).Select(y => new CoordinateDirection(new Coordinate(width,y), Coordinate.OffsetLeft)));
+
+        return beams.Max(beam => CalculateBeam(parsed, width, height, beam.Position, beam.Direction).Count());
+    }
+
+    private static HashSet<Coordinate> CalculateBeam(Dictionary<Coordinate, char> grid, int width, int height, Coordinate currentPosition, Coordinate currentDirection)
+    {
+       // var energized = new HashSet<Coordinate>();
+        var energizedDirections = new HashSet<(Coordinate position,Coordinate direction)>();
+        
+        var queue = new Queue<(Coordinate position, Coordinate direction)>();
+        queue.Enqueue((currentPosition, currentDirection));
+
+        while (queue.TryDequeue(out (Coordinate position, Coordinate direction) state))
+        {
+            var (oldPosition,direction) = state;
+            var position = oldPosition + direction;
+
+            if (position.X < 0 || position.X >= width || position.Y < 0 || position.Y >= height)
+            {
+                continue;
+            }
+
+            if (!energizedDirections.Add((position, direction)))
+            {
+                // already visited
+                continue;
+            }
+
+            switch (grid[position])
+            {
+                case '.':
+                    queue.Enqueue((position, direction));
+                    break;
+
+                case '/':
+                    queue.Enqueue((position, CalculateMirror(direction, false)));
+                    break;
+                case '\\':
+                    queue.Enqueue((position, CalculateMirror(direction, true)));
+                    break;
+                case '|':
+                    if (direction == Coordinate.OffsetLeft || direction == Coordinate.OffsetRight)
+                    {
+                        queue.Enqueue((position, Coordinate.OffsetUp));
+                        queue.Enqueue((position, Coordinate.OffsetDown));
+                    } else
+                    {
+                        queue.Enqueue((position, direction));
+                    }
+                    break;
+
+                case '-':
+                    if (direction == Coordinate.OffsetDown || direction == Coordinate.OffsetUp)
+                    {
+                        queue.Enqueue((position, Coordinate.OffsetLeft));
+                        queue.Enqueue((position, Coordinate.OffsetRight));                       
+                    }
+                    else
+                    {
+                        queue.Enqueue((position, direction));
+                    }
+                    break;
+
+
+                default: throw new Exception("Invalid input");
+            };
+        }
+
+        return energizedDirections.Select(t => t.position).ToHashSet();
+    }
+
+    private static void Print(HashSet<Coordinate> coordinates, int width, int height)
+    {
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                if (coordinates.Contains(new Coordinate(x, y)))
+                {
+                    Console.Write('.');
+
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static Coordinate CalculateSplitter(Coordinate direction, bool invert)
+    {
+        if (direction == Coordinate.OffsetLeft)
+        {
+            return invert ? Coordinate.OffsetUp : Coordinate.OffsetDown;
+        }
+        else if (direction == Coordinate.OffsetRight)
+        {
+            return invert ? Coordinate.OffsetDown : Coordinate.OffsetUp;
+        }
+        else if (direction == Coordinate.OffsetUp)
+        {
+            return invert ? Coordinate.OffsetLeft : Coordinate.OffsetRight;
+        }
+        else if (direction == Coordinate.OffsetDown)
+        {
+            return invert ? Coordinate.OffsetRight : Coordinate.OffsetLeft;
+        }
+        else
+        {
+            throw new Exception("Invalid direction");
+        }
+    }
+
+    private static Coordinate CalculateMirror(Coordinate direction, bool invert)
+    {
+
+        if (direction == Coordinate.OffsetLeft)
+        {
+            return invert ? Coordinate.OffsetUp : Coordinate.OffsetDown;
+        }
+        else if (direction == Coordinate.OffsetRight)
+        {
+            return invert ? Coordinate.OffsetDown : Coordinate.OffsetUp;
+        }
+        else if (direction == Coordinate.OffsetUp)
+        {
+            return invert ? Coordinate.OffsetLeft : Coordinate.OffsetRight;
+        }
+        else if (direction == Coordinate.OffsetDown)
+        {
+            return invert ? Coordinate.OffsetRight : Coordinate.OffsetLeft;
+        }
+        else
+        {
+            throw new Exception("Invalid direction");
+        }
+
+
+    }
+
+    private static (int width, int height, Dictionary<Coordinate, char>) ParseInputs(string[] inputs)
+    {
+
+        var grid = new Dictionary<Coordinate, char>();
+
+        for (var y = 0; y < inputs.Length; y++)
+        {
+            for (var x = 0; x < inputs[y].Length; x++)
+            {
+                grid[new Coordinate(x, y)] = inputs[y][x];
+            }
+        }
+        return (inputs[0].Length, inputs.Length, grid);
+    }
+}
---
+++ utilities/Coordinate.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC.Utilities;
+public readonly record struct Coordinate(int X, int Y)
+{
+    public static Coordinate OffsetLeft => new(-1, 0);
+    public static Coordinate OffsetRight => new(1, 0);
+    public static Coordinate OffsetUp => new(0, -1);
+    public static Coordinate OffsetDown => new(0, 1);
+    public static Coordinate OffsetUpLeft => new(-1, -1);
+    public static Coordinate OffsetUpRight => new(1, -1);
+    public static Coordinate OffsetDownLeft => new(-1, 1);
+    public static Coordinate OffsetDownRight => new(1, 1);
+    public static Coordinate operator +(Coordinate a, Coordinate b) => new(a.X + b.X, a.Y + b.Y);
+    public static Coordinate operator +(Coordinate a, (int X, int Y) b) => new(a.X + b.X, a.Y + b.Y);
+    public static Coordinate operator -(Coordinate a, Coordinate b) => new(a.X - b.X, a.Y - b.Y);
+    public static Coordinate operator -(Coordinate a, (int X, int Y) b) => new(a.X - b.X, a.Y - b.Y);
+    public static Coordinate operator *(Coordinate a, int multiplier) => new(a.X * multiplier, a.Y * multiplier);
+    public Coordinate Left => new(X - 1, Y);
+    public Coordinate Right => new(X + 1, Y);
+    public Coordinate Up => new(X, Y - 1);
+    public Coordinate Down => new(X, Y + 1);
+    public Coordinate UpLeft => new(X - 1, Y - 1);
+    public Coordinate UpRight => new(X + 1, Y - 1);
+    public Coordinate DownLeft => new(X - 1, Y + 1);
+    public Coordinate DownRight => new(X + 1, Y + 1);
+    /// <summary>Horizontal & Vertical</summary>
+    public Coordinate[] Neighbors => new[] { Left, Right, Up, Down };
+    /// <summary>Horizontal, Vertical & Diagonal</summary>
+    public Coordinate[] Adjacents => new[] { Left, Right, Up, Down, UpLeft, UpRight, DownLeft, DownRight };
+    /// <summary>Adjacent or on top</summary>
+    public bool IsAdjacent(Coordinate other) => Math.Abs(other.X - X) <= 1 && Math.Abs(other.Y - Y) <= 1;
+    /// <summary>Calculate Manhattan Distance</summary>
+    public int DistanceTo(Coordinate other) => Math.Abs(other.X - X) + Math.Abs(other.Y - Y);
+    /// <summary>Calculate Offset/Direction to target</summary>
+    public Coordinate OffsetTo(Coordinate other) => new((other.X - X == 0) ? 0 : (other.X - X < 0) ? -1 : 1, (other.Y - Y == 0) ? 0 : (other.Y - Y < 0) ? -1 : 1);
+    public Coordinate RotateLeft => new(Y, -X);
+    public Coordinate RotateRight => new(-Y, X);
+    public override string ToString() => $"{X},{Y}";
+}
