8f2bf23e6df179b97c59b860b96af88ed1bd15f3
Anton Rademaker <antonrademaker@gmail.com>
2024-01-12 15:41:25 +0100

    Day 18

--- utilities/Coordinate.cs
+++ utilities/Coordinate.cs
@@ -1,38 +1,41 @@
-﻿namespace AoC.Utilities;
-public readonly record struct Coordinate(int X, int Y)
-{
-    public static Coordinate OffsetLeft => new(-1, 0);
-    public static Coordinate OffsetRight => new(1, 0);
-    public static Coordinate OffsetUp => new(0, -1);
-    public static Coordinate OffsetDown => new(0, 1);
-    public static Coordinate OffsetUpLeft => new(-1, -1);
-    public static Coordinate OffsetUpRight => new(1, -1);
-    public static Coordinate OffsetDownLeft => new(-1, 1);
-    public static Coordinate OffsetDownRight => new(1, 1);
-    public static Coordinate operator +(Coordinate a, Coordinate b) => new(a.X + b.X, a.Y + b.Y);
-    public static Coordinate operator +(Coordinate a, (int X, int Y) b) => new(a.X + b.X, a.Y + b.Y);
-    public static Coordinate operator -(Coordinate a, Coordinate b) => new(a.X - b.X, a.Y - b.Y);
-    public static Coordinate operator -(Coordinate a, (int X, int Y) b) => new(a.X - b.X, a.Y - b.Y);
-    public static Coordinate operator *(Coordinate a, int multiplier) => new(a.X * multiplier, a.Y * multiplier);
-    public Coordinate Left => new(X - 1, Y);
-    public Coordinate Right => new(X + 1, Y);
-    public Coordinate Up => new(X, Y - 1);
-    public Coordinate Down => new(X, Y + 1);
-    public Coordinate UpLeft => new(X - 1, Y - 1);
-    public Coordinate UpRight => new(X + 1, Y - 1);
-    public Coordinate DownLeft => new(X - 1, Y + 1);
-    public Coordinate DownRight => new(X + 1, Y + 1);
+﻿using System.Numerics;
+
+namespace AoC.Utilities;
+
+public readonly record struct Coordinate<T>(T X, T Y) where T: INumber<T> { 
+
+    public static Coordinate<T> OffsetLeft => new(-T.One, T.Zero);
+    public static Coordinate<T> OffsetRight => new(T.One, T.Zero);
+    public static Coordinate<T> OffsetUp => new(T.Zero, -T.One);
+    public static Coordinate<T> OffsetDown => new(T.Zero, T.One);
+    public static Coordinate<T> OffsetUpLeft => new(-T.One, -T.One);
+    public static Coordinate<T> OffsetUpRight => new(T.One, -T.One);
+    public static Coordinate<T> OffsetDownLeft => new(-T.One, T.One);
+    public static Coordinate<T> OffsetDownRight => new(T.One, T.One);
+    public static Coordinate<T> operator +(Coordinate<T> a, Coordinate<T> b) => new(a.X + b.X, a.Y + b.Y);
+    public static Coordinate<T> operator +(Coordinate<T> a, (T X, T Y) b) => new(a.X + b.X, a.Y + b.Y);
+    public static Coordinate<T> operator -(Coordinate<T> a, Coordinate<T> b) => new(a.X - b.X, a.Y - b.Y);
+    public static Coordinate<T> operator -(Coordinate<T> a, (T X, T Y) b) => new(a.X - b.X, a.Y - b.Y);
+    public static Coordinate<T> operator *(Coordinate<T> a, T multiplier) => new(a.X * multiplier, a.Y * multiplier);
+    public Coordinate<T> Left => new(X - T.One, Y);
+    public Coordinate<T> Right => new(X + T.One, Y);
+    public Coordinate<T> Up => new(X, Y - T.One);
+    public Coordinate<T> Down => new(X, Y + T.One);
+    public Coordinate<T> UpLeft => new(X - T.One, Y - T.One);
+    public Coordinate<T> UpRight => new(X + T.One, Y - T.One);
+    public Coordinate<T> DownLeft => new(X - T.One, Y + T.One);
+    public Coordinate<T> DownRight => new(X + T.One, Y + T.One);
     /// <summary>Horizontal & Vertical</summary>
-    public Coordinate[] Neighbors => new[] { Left, Right, Up, Down };
+    public Coordinate<T>[] Neighbors => [Left, Right, Up, Down];
     /// <summary>Horizontal, Vertical & Diagonal</summary>
-    public Coordinate[] Adjacents => new[] { Left, Right, Up, Down, UpLeft, UpRight, DownLeft, DownRight };
+    public Coordinate<T>[] Adjacents => [Left, Right, Up, Down, UpLeft, UpRight, DownLeft, DownRight];
     /// <summary>Adjacent or on top</summary>
-    public bool IsAdjacent(Coordinate other) => Math.Abs(other.X - X) <= 1 && Math.Abs(other.Y - Y) <= 1;
+    public bool IsAdjacent(Coordinate<T> other) => T.Abs(other.X - X) <= T.One && T.Abs(other.Y - Y) <= T.One;
     /// <summary>Calculate Manhattan Distance</summary>
-    public int DistanceTo(Coordinate other) => Math.Abs(other.X - X) + Math.Abs(other.Y - Y);
+    public T DistanceTo(Coordinate<T> other) => T.Abs(other.X - X) + T.Abs(other.Y - Y);
     /// <summary>Calculate Offset/Direction to target</summary>
-    public Coordinate OffsetTo(Coordinate other) => new((other.X - X == 0) ? 0 : (other.X - X < 0) ? -1 : 1, (other.Y - Y == 0) ? 0 : (other.Y - Y < 0) ? -1 : 1);
-    public Coordinate RotateLeft => new(Y, -X);
-    public Coordinate RotateRight => new(-Y, X);
+    public Coordinate<T> OffsetTo(Coordinate<T> other) => new((other.X - X == T.Zero) ? T.Zero : (other.X - X < T.Zero) ? -T.One : T.One, (other.Y - Y == T.Zero) ? T.Zero : (other.Y - Y < T.Zero) ? -T.One : T.One);
+    public Coordinate<T> RotateLeft => new(Y, -X);
+    public Coordinate<T> RotateRight => new(-Y, X);
     public override string ToString() => $"{X},{Y}";
 }
