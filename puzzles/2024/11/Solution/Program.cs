7ae1ebacd873ff867130a877bf201141d0d471dc
Anton Rademaker <antonrademaker@gmail.com>
2024-12-11 10:10:28 +0100

    Day 11

---
+++ puzzles/2024/11/Solution/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Diagnostics;
+
+namespace Solution;
+
+public class Program
+{
+    private static readonly string[] inputFiles = ["input/example1.txt", "input/input.txt"];
+
+    static Dictionary<(long stones, int steps), long> Cache = [];
+
+    public static void Main(string[] args)
+    {
+        foreach (string file in inputFiles)
+        {
+            Console.WriteLine($"Reading: {file}");
+
+            var input = ParseFile(file);
+
+            var startTime = Stopwatch.GetTimestamp();
+
+            var answer1 = CalculateAnswer1(input);
+            var elapsedTime = Stopwatch.GetElapsedTime(startTime);
+            Console.WriteLine($"{file}: Answer 1: {answer1} ({elapsedTime.TotalMilliseconds}ms)");
+
+            startTime = Stopwatch.GetTimestamp();
+            var answer2 = CalculateAnswer2(input);
+            elapsedTime = Stopwatch.GetElapsedTime(startTime);
+
+            Console.WriteLine($"{file}: Answer 2: {answer2} ({elapsedTime.TotalMilliseconds}ms)");
+        }
+    }
+
+    static Input ParseFile(string file)
+    {
+        return new Input(File.ReadAllText(file));
+    }
+
+    public static long CalculateAnswer1(Input input)
+    {
+        var steps = 25;
+
+        return input.Map.Sum(stones => CalculateStones(stones, steps));
+    }
+
+    public static long CalculateAnswer2(Input input)
+    {
+        var steps = 75;
+
+        return input.Map.Sum(stones => CalculateStones(stones, steps));
+    }
+
+    public static long CalculateStones(long stones, int steps)
+    {
+        if (Cache.TryGetValue((stones, steps), out var value))
+        {
+            return value;
+        }
+
+        var result = 1L;
+        if (steps == 0)
+        {
+            return result;
+        }
+        else
+        {
+            if (stones == 0)
+            {
+                result = CalculateStones(1, steps - 1);
+
+                Cache[(stones, steps)] = result;
+
+                return result;
+            }
+            else
+            {
+                var length = 1 + (long)Math.Floor(Math.Log10(stones));
+
+                if (length % 2 == 0)
+                {
+
+                    var left = stones / (long)Math.Pow(10, length / 2);
+                    var right = stones % (long)Math.Pow(10, length / 2);
+
+                    result = CalculateStones(left, steps - 1) + CalculateStones(right, steps - 1);
+                }
+                else
+                {
+
+                    result = CalculateStones(stones * 2024, steps - 1);
+                }
+            }
+
+        }
+        Cache[(stones, steps)] = result;
+
+        return result;
+    }
+}
+
+public readonly ref struct Input(string input)
+{
+    public readonly long[] Map = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+    public long Get(int loc)
+    {
+        return Map[loc];
+    }
+}
---
+++ puzzles/2024/11/TestProject/UnitTest.cs
@@ -0,0 +1,31 @@
+﻿namespace TestProject;
+
+using FluentAssertions;
+using Solution;
+
+public class UnitTest
+{
+    private const string Example1 = "125 17";
+    
+
+    [Theory]
+    [InlineData(Example1, 55312)]    
+    public void TestExamplePart1(string inputString, long expected)
+    {
+        var input = new Input(inputString);
+
+        Program.CalculateAnswer1(input).Should().Be(expected);
+    }
+
+    //[Theory]
+    ////[InlineData(Example1Part2, 3)]
+    ////[InlineData(Example2Part2, 13)]
+    ////[InlineData(Example3Part2, 227)]
+    //[InlineData(Example1, 81)]
+    //public void TestExamplePart2(string inputString, int expected)
+    //{
+    //    var input = new Input(inputString);
+
+    //    Program.CalculateAnswer2(input).Should().Be(expected);
+    //}
+}
