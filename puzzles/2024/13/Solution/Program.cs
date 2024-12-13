007d682e14387d386ae82627c4c8f7bf980980fd
Anton Rademaker <antonrademaker@gmail.com>
2024-12-13 12:20:48 +0100

    Start of day 13

---
+++ puzzles/2024/13/Solution/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+using Coordinate = AoC.Utilities.Coordinate<int>;
+
+namespace Solution;
+
+public partial class Program
+{
+    private static readonly string[] inputFiles = ["input/example1.txt", "input/input.txt"];
+
+    public static void Main(string[] args)
+    {
+        foreach (string file in inputFiles)
+        {
+            Console.WriteLine($"Reading: {file}");
+
+            var input = ParseFile(file);
+
+            var startTime = Stopwatch.GetTimestamp();
+
+            var answer1 = CalculateAnswer1(input);
+            var elapsedTime = Stopwatch.GetElapsedTime(startTime);
+            Console.WriteLine($"{file}: Answer 1: {answer1} ({elapsedTime.TotalMilliseconds}ms)");
+
+            startTime = Stopwatch.GetTimestamp();
+            var answer2 = CalculateAnswer2(input);
+            elapsedTime = Stopwatch.GetElapsedTime(startTime);
+
+            Console.WriteLine($"{file}: Answer 2: {answer2} ({elapsedTime.TotalMilliseconds}ms)");
+        }
+    }
+
+    static Input ParseFile(string file)
+    {
+        return new Input(File.ReadAllLines(file));
+    }
+
+    public static long CalculateAnswer1(Input input)
+    {
+       var machines = input.GetMachines();
+
+        var result = 0;
+
+        foreach (var machine in machines) {
+            // a * X.A + b * X.B = X.Price
+            // a * Y.A + b * Y.B = Y.Price
+
+
+            // a = Y.Price - b * Y.B / Y.A
+
+
+            // (Y.Price - b * Y.B / Y.A) * X.A + b * X.B = X.Price
+
+            // X.A * Y.Price - b * Y.B + b * X.B * Y.A = X.Price * Y.A
+
+
+
+
+
+        }
+
+
+
+        return result;
+    }
+
+
+
+    public static long CalculateAnswer2(Input input)
+    {
+        return 0;
+    }
+
+
+    public static readonly Regex ParseRegex = Parser();
+
+    [GeneratedRegex(@"X([\+=])(?<X>\d+), Y([\+=])(?<Y>\d+)", RegexOptions.Compiled)]
+    private static partial Regex Parser();
+}
+
+public readonly ref struct Input(string[] input)
+{
+    private readonly string[] StringInput = input;
+    
+    public List<Machine> GetMachines()
+    {
+        List<Machine> machines = [];
+
+        for(int i = 0; i < StringInput.Length; i+= 4)
+        {            
+            machines.Add(new Machine(GetCoordinate(StringInput[i]), GetCoordinate(StringInput[i+1]), GetCoordinate(StringInput[i+2])));
+        }
+
+        return machines;
+    }
+
+    private Coordinate GetCoordinate(string input)
+    {
+        var coords = Program.ParseRegex.Match(input);
+
+        return new Coordinate(int.Parse(coords.Groups["X"].Value), int.Parse(coords.Groups["Y"].Value));
+    }
+}
+
+public readonly record struct Machine(Coordinate A, Coordinate B, Coordinate Price)
+{
+    
+}
---
+++ puzzles/2024/13/TestProject/UnitTest.cs
@@ -0,0 +1,70 @@
+﻿namespace TestProject;
+
+using FluentAssertions;
+using Solution;
+
+public class UnitTest
+{
+    private const string Example1 = """
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+""";
+    private const string Example2 = """
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+""";
+
+    private const string Example3 = """
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+""";
+
+    private const string Example4 = """
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+""";
+
+    private const string Example5 = """
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+""";
+
+    [Theory]
+    [InlineData(Example1, 140)]
+    [InlineData(Example2, 1930)]
+    [InlineData(Example3, 4)]
+    [InlineData(Example4, 12)]
+    [InlineData(Example5, 12)]
+    public void TestExample1Part1(string inputString, long expected)
+    {
+        var input = new Input(inputString.Split(Environment.NewLine));
+
+        Program.CalculateAnswer1(input).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(Example1, 80)]
+    public void TestExamplePart2(string inputString, int expected)
+    {
+        var input = new Input(inputString.Split(Environment.NewLine));
+
+        Program.CalculateAnswer2(input).Should().Be(expected);
+    }
+}
