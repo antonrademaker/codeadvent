119afc61de65fde0f7707dd6f3fc4e2d4dbf65b5
Anton Rademaker <antonrademaker@gmail.com>
2024-12-22 12:49:28 +0100

    Day 22

---
+++ puzzles/2024/22/Solution/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks.Sources;
+using AoC.Utilities;
+using Microsoft.Extensions.ObjectPool;
+using Coordinate = AoC.Utilities.Coordinate<int>;
+
+namespace Solution;
+
+public partial class Program
+{
+    private static readonly List<string> inputFiles = ["input/example1.txt", "input/input.txt"];
+
+    private const int MaxOptions = 19;
+    private const int PowerMaxOptionsTo3 = MaxOptions * MaxOptions * MaxOptions;
+
+    public static void Main(string[] _)
+    {
+        foreach (var file in inputFiles)
+        {
+            Console.WriteLine($"Reading: {file}");
+
+            var input1 = ParseFile(file);
+
+            var startTime = Stopwatch.GetTimestamp();
+
+            var answer1 = CalculateAnswer1(input1);
+            var elapsedTime = Stopwatch.GetElapsedTime(startTime);
+            Console.WriteLine($"{file}: Answer 1: {answer1} ({elapsedTime.TotalMilliseconds}ms)");
+            Console.WriteLine();
+
+            var input2 = ParseFile(file);
+
+            startTime = Stopwatch.GetTimestamp();
+            var answer2 = CalculateAnswer2(input2);
+            elapsedTime = Stopwatch.GetElapsedTime(startTime);
+
+            Console.WriteLine($"{file}: Answer 2: {answer2} ({elapsedTime.TotalMilliseconds}ms)");
+        }
+    }
+
+    private static Input ParseFile(string file)
+    {
+        return new Input(File.ReadAllText(file));
+    }
+
+    public static long CalculateAnswer1(Input input)
+    {
+        return Calculate(input, 2000).part1;
+    }
+
+    public static long CalculateAnswer2(Input input)
+    {
+        return Calculate(input, 2000).part2;
+    }
+
+    public static (long part1, long part2) Calculate(Input input, int steps)
+    {
+        ulong part1 = 0;
+
+        DefaultDictionary<int, int> bananas = [];
+        DefaultDictionary<int, int> occurred = [];
+
+        for (var inputIndex = 0; inputIndex < input.Lines.Length; inputIndex++)
+        {
+            ulong secret = (ulong)input.Lines[inputIndex];
+            int lastPrice = (int)(secret % 10);
+
+            int delta;
+            var sequenceIndex = 0;
+
+            for (var iteration = 0; iteration < steps; iteration++)
+            {
+                secret = Prune(Mix(secret << 6, secret));
+                secret = Prune(Mix(DivideBy32(secret), secret));
+                secret = Prune(Mix(secret << 11, secret));
+
+                var price = (int)(secret % 10);
+
+                (delta, lastPrice) = (price - lastPrice, price);
+
+                sequenceIndex = (sequenceIndex % PowerMaxOptionsTo3) * MaxOptions + (delta + 9);
+                // skip first iterations and only register once per monkey
+                if (iteration >= 3 && occurred[sequenceIndex] != inputIndex + 1)
+                {
+                    bananas[sequenceIndex] += price;
+                    occurred[sequenceIndex] = inputIndex + 1;
+                }
+            }
+
+            part1 += secret;
+        }
+
+        var part2 = bananas.Any() ? bananas.Values.Max() : 0;
+
+        return ((long)part1, part2);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ulong DivideBy32(ulong value)
+    {
+        return value >> 5;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ulong Mix(ulong newValue, ulong secretNumber)
+    {
+        return newValue ^ secretNumber;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ulong Prune(ulong value)
+    {
+        return value & 16777215;
+    }
+}
+
+public readonly struct Input(string input)
+{
+    public readonly int[] Lines = [.. input.Split(Environment.NewLine).Select(int.Parse)];
+}
---
+++ puzzles/2024/22/TestProject/UnitTest.cs
@@ -0,0 +1,77 @@
+﻿namespace TestProject;
+
+using FluentAssertions;
+using Solution;
+
+public class UnitTest
+{
+    private const string Example1 = """
+1
+10
+100
+2024
+""";
+
+    [Theory]
+    [InlineData(Example1, 37327623)]
+    public void TestExamplesPart1(string inputString, long expected)
+    {
+        var input = new Input(inputString);
+
+        Program.CalculateAnswer1(input).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("123", 0, 123)]
+    [InlineData("123", 1, 15887950)]
+    [InlineData("123", 2, 16495136)]
+    [InlineData("123", 3, 527345)]
+    [InlineData("123", 4, 704524)]
+    [InlineData("123", 5, 1553684)]
+    [InlineData("123", 6, 12683156)]
+    [InlineData("123", 7, 11100544)]
+    [InlineData("123", 8, 12249484)]
+    [InlineData("123", 9, 7753432)]
+    [InlineData("123", 10, 5908254)]
+    public void TestShortExamplesPart1(string inputString, int steps, long expected)
+    {
+        var input = new Input(inputString);
+
+        Program.Calculate(input, steps).part1.Should().Be(expected);
+    }
+
+    [Fact]
+    public void TestMix()
+    {
+        Program.Mix(42, 15).Should().Be(37);
+    }
+
+
+    [Fact]
+    public void TestPrune()
+    {
+        Program.Prune(100000000).Should().Be(16113920);
+    }
+
+
+    private const string Example2 = """
+1
+2
+3
+2024
+""";
+    private const string Example3 = """
+123
+""";
+    [Theory]
+   [InlineData(Example2, 23)]
+    //[InlineData(Example3, 23)]
+    public void TestSolves(string inputString, int expected)
+    {
+        var input = new Input(inputString);
+
+        Program.Calculate(input, 2000).part2.Should().Be(expected);
+    }
+
+
+}
