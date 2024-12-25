5dabda9fe1538c5196a547aaa0b0c5790bff7337
Anton Rademaker <antonrademaker@gmail.com>
2024-12-25 21:09:28 +0100

    Day 23 - part solved by hand

--- puzzles/2024/24/Solution/Program.cs
+++ puzzles/2024/24/Solution/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Runtime;
 using System.Runtime.InteropServices;
+using System.Text;
 using AoC.Utilities;
 
 namespace Solution;
@@ -9,9 +10,10 @@ namespace Solution;
 public partial class Program
 {
     private static readonly List<string> inputFiles = [
-        "input/example1.txt",
-        "input/example2.txt",
-        "input/input.txt"
+        //"input/example1.txt",
+        //"input/example2.txt",
+        "input/input.txt",
+        "input/fixed.txt"
 
         ];
 
@@ -71,8 +73,6 @@ public partial class Program
         return GetZWireValue(input);
     }
 
-
-
     public static int CalculateAnswer2(Input input)
     {
         var xValue = GetXWireBitValue(input);
@@ -80,20 +80,33 @@ public partial class Program
 
         var expectedOutput = xValue + yValue;
 
-        var expectedBytes = new BitArray(BitConverter.GetBytes(expectedOutput));
+        var actualOutput = CalculateAnswer1(input);
 
+        var difference = expectedOutput - actualOutput;
+        Console.WriteLine($"Difference: {difference}");
+
+        var wrong = new BitArray(BitConverter.GetBytes(difference));
+        for (var i = 0; i < wrong.Length; i++)
+        {
+            if (wrong[i])
+            {
+                //    wrongWires.Add($"z{i:00}");
+                Console.WriteLine($"z{i:00}");
+                ///       currentChanges.Add(new ChangeOption(ChangeType.One, [$"z{i:00}"]));
+            }
+        }
+
+        // Visualize
+
+        Console.WriteLine(input.Visualize());
+
+        // solved by hand: css,cwt,gdd,jmv,pqt,z05,z09,z37
         return 0;
     }
-
 
     private static long GetZWireValue(Input input)
     {
         return ToLong(input.WireValues.Where(t => t.Key.StartsWith('z')).OrderBy(x => x.Key).Select(t => t.Value).ToArray());
-    }
-
-    private static BitArray GetZWireBitValue(Input input)
-    {
-        return new BitArray(input.WireValues.Where(t => t.Key.StartsWith('z')).OrderBy(x => x.Key).Select(t => t.Value).ToArray());
     }
 
     private static long GetXWireBitValue(Input input)
@@ -123,17 +136,33 @@ public partial class Program
     }
 }
 
-public abstract class Operation(string outputWire)
+public abstract class Operation(string outputWire, string input1, string input2)
 {
-    protected readonly string outputWire = outputWire;
+    public readonly string OutputWire = outputWire;
+    public readonly string Input1 = input1;
+    public readonly string Input2 = input2;
 
     public abstract bool Push(bool input, out bool output, out string outputWire);
+
+    public abstract string Type { get; }
+
+    public string VisualizeOperation()
+    {
+        return $"\"{Type.ToUpperInvariant()}\" as Operation{OutputWire}";
+    }
+
+    public string VisualizeWires()
+    {
+        return $"{Input1} -> Operation{OutputWire}\r\n{Input2} -> Operation{OutputWire}\r\nOperation{OutputWire} -> {OutputWire}";
+    }
 }
 
-public class AndOperation(string output) : Operation(output)
+public class AndOperation(string output, string input1, string input2) : Operation(output, input1, input2)
 {
     private readonly bool[] inputs = new bool[2];
     private int inputCount;
+
+    public override string Type => "And";
 
     public override bool Push(bool input, out bool output, out string outputWire)
     {
@@ -141,7 +170,7 @@ public class AndOperation(string output) : Operation(output)
         if (inputCount == 2)
         {
             output = inputs[0] && inputs[1];
-            outputWire = this.outputWire;
+            outputWire = this.OutputWire;
             return true;
         }
         output = false;
@@ -150,10 +179,13 @@ public class AndOperation(string output) : Operation(output)
     }
 }
 
-public class OrOperation(string output) : Operation(output)
+public class OrOperation(string output, string input1, string input2) : Operation(output, input1, input2)
 {
     private readonly bool[] inputs = new bool[2];
     private int inputCount;
+
+    public override string Type => "Or";
+
 
     public override bool Push(bool input, out bool output, out string outputWire)
     {
@@ -161,7 +193,7 @@ public class OrOperation(string output) : Operation(output)
         if (inputCount == 2)
         {
             output = inputs[0] || inputs[1]; // can optimize this
-            outputWire = this.outputWire;
+            outputWire = this.OutputWire;
             return true;
         }
         output = false;
@@ -171,10 +203,12 @@ public class OrOperation(string output) : Operation(output)
     }
 }
 
-public class XorOperation(string output) : Operation(output)
+public class XorOperation(string output, string input1, string input2) : Operation(output, input1, input2)
 {
     private readonly bool[] inputs = new bool[2];
     private int inputCount;
+
+    public override string Type => "Xor";
 
     public override bool Push(bool input, out bool output, out string outputWire)
     {
@@ -182,7 +216,7 @@ public class XorOperation(string output) : Operation(output)
         if (inputCount == 2)
         {
             output = inputs[0] ^ inputs[1];
-            outputWire = this.outputWire;
+            outputWire = this.OutputWire;
             return true;
         }
         output = false;
@@ -192,21 +226,18 @@ public class XorOperation(string output) : Operation(output)
     }
 }
 
-
-
-
-
-
 public readonly struct Input
 {
     public readonly string[] Lines;
 
     public readonly DefaultFactoryDictionary<string, List<Operation>> Operators = new(x => []);
+    public readonly DefaultFactoryDictionary<string, List<Operation>> OperatorsByOutput = new(x => []);
+
+    public readonly List<Operation> OperatorList = [];
 
     public readonly Dictionary<string, bool> WireValues = [];
 
     public readonly string LastZWire;
-       
 
     public Input(string input)
     {
@@ -237,16 +268,37 @@ public readonly struct Input
                 var split = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
                 Operation operation = split[1] switch
                 {
-                    "AND" => new AndOperation(split[4]),
-                    "OR" => new OrOperation(split[4]),
-                    "XOR" => new XorOperation(split[4]),
+                    "AND" => new AndOperation(split[4], split[0], split[2]),
+                    "OR" => new OrOperation(split[4], split[0], split[2]),
+                    "XOR" => new XorOperation(split[4], split[0], split[2]),
                     _ => throw new InvalidOperationException()
                 };
                 Operators[split[0]].Add(operation);
 
                 Operators[split[2]].Add(operation);
 
+                OperatorsByOutput[split[4]].Add(operation);
+
+                OperatorList.Add(operation);
             }
         }
+    }
+
+    public string Visualize()
+    {
+        // https://playground.diagram.codes/
+        var sb = new StringBuilder();
+        sb.AppendLine("NO ARROWS");
+        foreach (var w in OperatorList)
+        {
+            sb.AppendLine(w.VisualizeOperation());
+        }
+
+        foreach (var w in OperatorList)
+        {
+            sb.AppendLine(w.VisualizeWires());
+        }
+        
+        return sb.ToString();
     }
 }
